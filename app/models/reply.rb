775ef2fb689aadda86754c9d112142fff28f6162
jesical <chensongL516@gmail.com>
2014-04-12 18:11:30 +0800

    change reply edit style to summernote

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,6 +2,7 @@ class Reply < ActiveRecord::Base
 
   belongs_to :topic,:counter_cache => true,:inverse_of => :replies
   belongs_to :user, :counter_cache => true,:inverse_of => :replies
+  has_many :pictures,as: :imageable,:dependent => :destroy
 
   validates_presence_of :user,:topic,:body
 end

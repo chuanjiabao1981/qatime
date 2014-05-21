bf3e72f1391dd8fe6958d528377865c64cf9f0b1
jesical <chensongL516@gmail.com>
2014-05-21 13:35:11 +0800

    finish video play

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,10 +11,18 @@ class CoursesController < ApplicationController
     unless @lesson
       @lesson   = @course.lessons.order(created_at: :asc).first
     end
+
+    generate_download_token
   end
 
   def node
     @node = Node.find(params[:id])
     @courses = Course.where(node_id: @node.id)
+  end
+
+  def generate_download_token
+    @download_token = Qiniu::RS.generate_download_token :expires_in => 300,
+                                                        :pattern => "qatime.qiniudn.com/uploads/*"
+    @download_token
   end
 end

a5948e4477141b5b612482339bd4bcca6f278f18
guowei <chuanjiabao1981@gmail.com>
2014-05-27 07:19:09 +0800

    course purchase

--- app/models/section.rb
+++
@@ -1,8 +0,0 @@
-class Section < ActiveRecord::Base
-
-  has_many :nodes, :dependent => :destroy
-
-  validates_presence_of :name
-  validates_uniqueness_of :name
-
-end
--- db/migrate/20140223020328_create_sections.rb
+++
@@ -1,8 +0,0 @@
-class CreateSections < ActiveRecord::Migration
-  def change
-    create_table :sections do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end

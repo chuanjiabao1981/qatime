bc37d668f0a348e0da0c791d27eed7d8ab69cc8c
jesical <chensongL516@gmail.com>
2014-06-15 11:52:13 +0800

    fix bug and separate online/test config

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -12,11 +12,16 @@ class VideoUploader < CarrierWave::Uploader::Base
     if original_filename
       # current_path 是 Carrierwave 上传过程临时创建的一个文件，有时间标记，所以它将是唯一的
       @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
+
+      path_elements = original_filename.split('.')
+      extension = path_elements.last if path_elements.size > 1
+
       if not file.extension.empty?
-        "#{@name}.#{file.extension}"
+        "#{@name}.#{extension}"
       else
         "#{@name}.mp4"
       end
+
     end
   end
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,12 @@
 CarrierWave.configure do |config|
   config.storage :aliyun
-  config.aliyun_access_id = "uwTxHd9b4GNSQUWn"
-  config.aliyun_access_key = 'v0BvM2K2ClP6kqr10xWqUPZswCKH4L'
-  config.aliyun_bucket = "qatime"
+  config.aliyun_access_id = APP_CONFIG[:aliyun_ac]
+  config.aliyun_access_key = APP_CONFIG[:aliyun_ak]
+  if Rails.env.production?
+    config.aliyun_bucket = APP_CONFIG[:bucket_online]
+  else
+    config.aliyun_bucket = APP_CONFIG[:bucket_test]
+  end
   config.aliyun_internal = false
   config.aliyun_area = "cn-beijing"
 end

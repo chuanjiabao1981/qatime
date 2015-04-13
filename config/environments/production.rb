b07a1bbdf122be26aa91ede6975d699995a08b6a
chuanjiabao1981 <chuanjiabao1981@gmail.com>
2015-04-13 23:49:17 +0000

    change video player from jwplayer to videojs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Qatime::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += %w( *.scss *.js )
+  config.assets.precompile += %w( *.scss *.js  .svg .eot .woff .ttf)
 
   # Generate digests for assets URLs.
   config.assets.digest = true

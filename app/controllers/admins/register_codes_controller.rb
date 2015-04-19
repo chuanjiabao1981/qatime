c28bb74e912abc3c807096a40472096cb9ddfa4e
chuanjiabao <chuanjiabao1981@gmail.com>
2015-04-19 01:35:17 +0000

    delete unused file

--- app/controllers/admins/register_codes_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admins::RegisterCodesController < ApplicationController
-  layout "admin_home"
-  respond_to :html
-
-  def index
-    @register_codes = RegisterCode.all.paginate(page: params[:page],:per_page => 10)
-  end
-
-end

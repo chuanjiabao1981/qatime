c821ddf7b93e0c100b24bc453fc328e803b3c667
chuanjiabao <chuanjiabao1981@gmail.com>
2015-04-20 22:08:07 +0000

    close permission for student

--- app/controllers/vip_classes_controller.rb
+++ app/controllers/vip_classes_controller.rb
@@ -3,8 +3,6 @@ class VipClassesController < ApplicationController
 
   def show
     @questions = Question.includes(:learning_plan).by_vip_class(params[:id]).order("created_at desc").paginate(page: params[:page],:per_page => 10)
-    #@questions = Question.by_vip_class(params[:id]).order("created_at desc").paginate(page: params[:page],:per_page => 10)
-    #@questions = Question.includes(learning_plan: :teachers).by_vip_class(params[:id]).order("created_at desc").paginate(page: params[:page],:per_page => 10)
 
 
   end
--- test/integration/question_create_test.rb
+++ test/integration/question_create_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class QuestionCreateTest < ActionDispatch::IntegrationTest
   test "the truth" do
-    get new_session_path
-    assert_response :success
+    teacher1 = users(:teacher1)
+    visit new_session_path
+    fill_in :user_email,with: teacher1.email
+    fill_in :user_password,with: 'password'
+    click_button '登录'
+    visit questions_path
+    print page.html
   end
 end

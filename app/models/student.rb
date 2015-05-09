92f3d99e6db05cd5d34f5a06ecb431a7ef50f43d
chuanjiabao <chuanjiabao1981@gmail.com>
2015-05-09 11:31:00 +0000

    add english

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,9 +4,10 @@ class Student < User
   has_many :recharge_records
 
   has_many :courses,:through => :course_purchase_records
-  has_many :course_purchase_records
-  has_many :questions
-  has_many :learning_plans ,-> { order 'created_at desc' }
+  has_many :course_purchase_records ,:dependent => :destroy
+  has_many :questions,:dependent => :destroy
+  has_many :learning_plans ,-> { order 'created_at desc' } ,:dependent => :destroy
+
   has_many :valid_learning_plans , ->{where("? between begin_at AND end_at", Time.zone.now.to_date) },class_name: 'LearningPlan'
 
   has_many :not_finished_learning_plans, -> {where("? <= end_at",Time.zone.now.to_date)},class_name:'LearningPlan'

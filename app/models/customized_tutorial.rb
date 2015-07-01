96b45dd7dfd198d741e30612d1bfb9231417c99d
chuanjiabao <chuanjiabao1981@gmail.com>
2015-07-01 21:35:42 +0000

    customized course

--- app/models/customized_tutorial.rb
+++ app/models/customized_tutorial.rb
@@ -1,11 +1,9 @@
 class CustomizedTutorial < ActiveRecord::Base
-  require 'carrierwave/orm/activerecord'
-  include ActiveModel::Dirty
-
-  include VideoConvert
 
   belongs_to :teacher
   belongs_to :customized_course
 
   has_one    :video,:dependent => :destroy,as: :videoable
+
+  validates_presence_of :title
 end
--- test/models/customized_tutorial_test.rb
+++ test/models/customized_tutorial_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class CustomizedTutorialTest < ActiveSupport::TestCase
   test "customized tutorial valid" do
     customized_tutorial1 = customized_tutorials(:customized_tutorial1)
-    #puts customized_tutorial1.valid?
-    #puts customized_tutorial1.errors.full_messages
+    assert customized_tutorial1.valid?,customized_tutorial1.errors.full_messages
+    assert_not_nil customized_tutorial1.video
+    assert customized_tutorial1.video.valid?,customized_tutorial1.video.errors.full_messages
+    # puts customized_tutorial1.build_video.videoable_type
   end
 end

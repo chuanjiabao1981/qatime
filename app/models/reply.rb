a28fa8a5fa9619364939f125df74dc9fa20ec609
chuanjiabao <chuanjiabao1981@gmail.com>
2015-08-13 22:23:55 +0000

    reply add video

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -10,6 +10,7 @@ class Reply < ActiveRecord::Base
   belongs_to :author, :class_name => "User",:counter_cache => true,:inverse_of => :replies
 
   has_many :pictures,as: :imageable
+  has_one :video,as: :videoable
 
   validates_presence_of :author,:topic
 

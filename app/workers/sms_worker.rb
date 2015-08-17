482f34e155cb8b7e34001b45f1f0d3c57920bc93
chuanjiabao <chuanjiabao1981@gmail.com>
2015-08-17 00:26:11 +0000

    some change

--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -92,7 +92,7 @@ class SmsWorker
     return unless topic
     # 只有是老师回复学生才需要通知
     return unless topic.author.student? and reply.author.teacher?
-    mobile        = reply.author.mobile
+    mobile        = topic.author.mobile
     message_body  = "【答疑时间】#{topic.author.name}，你好，#{reply.author.name}回复了你在#{topic.topicable.model_name.human}发起讨论，请关注#{Time.zone.now.strftime("%Y-%m-%d %H:%M:%S")}，谢谢。"
     _send_message do
       send_message(mobile,message_body)

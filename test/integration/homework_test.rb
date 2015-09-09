343522c443209015b9568201999bf2f9548fb7a2
chuanjiabao <chuanjiabao1981@gmail.com>
2015-09-09 10:56:44 +0000

    homework test

--- test/integration/homework_test.rb
+++ test/integration/homework_test.rb
@@ -7,23 +7,62 @@ class HomeworkIntegrateTest < LoginTestBase
   end
 
   test 'show page' do
-    #user_session = @teacher_session
-    #show_path    =
-    #user_session.get show_path
-    #user_session.assert_response :success
-    #user_session.assert_select 'a[href=?]',homework_path(@homework1),1
-    #user_session.assert_select 'a[href=?]',homework_path(@homework2),1
-    #puts user_session.response.body
-    show_page(@teacher_session,homeworks_customized_course_path(@customized_course))
 
+    #puts user_session.response.body
+    show_page(@teacher,@teacher_session,homeworks_customized_course_path(@customized_course))
+    show_page(@student,@student_session,homeworks_customized_course_path(@customized_course))
   end
 
+  test 'new page' do
+    new_page(@teacher,@teacher_session,new_customized_course_homework_path(@customized_course))
+    new_page(@student,@student_session,new_customized_course_homework_path(@customized_course))
+  end
+
+  test 'update page' do
+    update_path = customized_course_homework_path(@customized_course,@homework1)
+
+    update_page(@teacher,@teacher_session,update_path)
+    update_page(@student,@student_session,update_path)
+  end
 
   private
-  def show_page(user_session,show_path)
+
+  def update_page(user,user_session,update_path)
+    if user.teacher?
+      title = "xdfstitleeeeeee update title update"
+      user_session.put update_path,homework:{title: title,content: "xxxxxxxxxxxxxx"}
+      user_session.assert_redirected_to homework_path(@homework1)
+      user_session.follow_redirect!
+      user_session.assert_select 'h4',title
+    else
+      user_session.assert_redirected_to get_home_url(user)
+
+    end
+  end
+  def new_page(user,user_session,new_path)
+    if user.teacher?
+      user_session.get new_path
+      user_session.assert_response :success
+      user_session.assert_select 'form[action=?]',customized_course_homeworks_path(@customized_course),1
+      user_session.assert_template 'homeworks/new','homeworks/_form'
+    else
+      user_session.assert_redirected_to get_home_url(user)
+
+    end
+  end
+  def show_page(user,user_session,show_path)
     user_session.get show_path
     user_session.assert_response :success
     user_session.assert_select 'a[href=?]',homework_path(@homework1),1
     user_session.assert_select 'a[href=?]',homework_path(@homework2),1
+    if user.teacher?
+      user_session.assert_select 'a[href=?]', new_customized_course_homework_path(@customized_course),1
+      user_session.assert_select 'a[href=?]', edit_homework_path(@homework1),1
+      user_session.assert_select 'a[href=?]', edit_homework_path(@homework2),1
+    else
+      user_session.assert_select 'a[href=?]', new_customized_course_homework_path(@customized_course),0
+      user_session.assert_select 'a[href=?]', edit_homework_path(@homework1),0
+      user_session.assert_select 'a[href=?]', edit_homework_path(@homework2),0
+    end
   end
 end

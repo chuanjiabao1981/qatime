f84959151b03d47e2a704d2902a57f2af3e76721
chuanjiabao <chuanjiabao1981@gmail.com>
2015-09-28 22:36:39 +0000

    no resolve homework

--- app/models/customized_course.rb
+++ app/models/customized_course.rb
@@ -9,7 +9,10 @@ class CustomizedCourse < ActiveRecord::Base
 
   attr_accessor :s_category,:s_school,:s_subject
 
-
+  # has_many timeout_to_solve_homework, lambda { where(solutions_count: 0)},class_name: "Homework"
+  def timeout_to_solve_homeworks
+    self.homeworks.where("created_at <?",3.day.ago).where('solutions_count = 0')
+  end
   def name
     "#{CustomizedCourse.model_name.human}-#{self.created_at.strftime("%Y-%m-%d")}"
   end
--- test/models/customized_course_test.rb
+++ test/models/customized_course_test.rb
@@ -10,4 +10,12 @@ class CustomizedCourseAATest < ActiveSupport::TestCase
 
   end
 
+  test "timeout_to_solve_home" do
+    cc = customized_courses(:customized_course3)
+    assert cc.valid?
+    assert cc.homeworks.first.valid?
+    assert cc.timeout_to_solve_homeworks.count == 1
+
+  end
+
 end

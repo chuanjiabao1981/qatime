61c1cd475805ba372de5ed6b9e04cdd15ca73ffe
chuanjiabao <chuanjiabao1981@gmail.com>
2015-10-08 23:55:03 +0000

    add account and cash operation record

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -5,5 +5,9 @@ class AccountTest < ActiveSupport::TestCase
 
   test 'withdraw valid' do
     a = Account.new
+    a.errors.add(:money,"12341234134")
+    puts a.errors.full_messages
+    assert a.valid?,a.errors.full_messages
+
   end
 end

f6dfa7f2e1fe8e1de2270c37ad0d95da6dd49b16
chuanjiabao <chuanjiabao1981@gmail.com>
2015-11-26 10:29:58 +0000

    add last operator to solution /refactory active record / refactory active record notification

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,18 @@
 class Comment < ActiveRecord::Base
 
-  include QaCustomizedCourseActionRecord
-  include QaCustomizedCourseActionNotification
+  # include QaCustomizedCourseActionRecord
+  # include QaCustomizedCourseActionNotification
+
+  has_many :customized_course_comment_records ,as: :actionable,dependent: :destroy do
+    def build(attributes={})
+      if defined? proxy_association.owner.customized_course_id  and proxy_association.owner.customized_course_id
+        attributes[:customized_course_id]        = proxy_association.owner.customized_course_id
+      end
+      super attributes
+    end
+  end
+
+
 
   belongs_to :commentable, :polymorphic => true,:counter_cache => true,:inverse_of => :comments
   belongs_to :customized_course
@@ -15,4 +26,12 @@ class Comment < ActiveRecord::Base
     self.author_id
   end
 
+
+  after_create :__create_action_record
+  private
+  private
+  def __create_action_record
+    a = self.customized_course_comment_records.build(name: :create,operator_id: self.operator_id)
+    a.save
+    end
 end
--- lib/tasks/qatime_status.rake
+++ lib/tasks/qatime_status.rake
@@ -9,10 +9,7 @@ task :qatime_status => :environment do
     a = [a.id,a.actionable_id]
     kk.append(a)
   end
-  kk.each do |k|
-    puts k.size
-  end
-  puts "=========================="
+
   kk.each do |k|
     a = Notification.find_by_notificationable_id(k[1])
     next unless a
@@ -21,7 +18,9 @@ task :qatime_status => :environment do
     # a.to_json
     a.save
   end
-  # Notification.all.each do |n|
-  #   puts "#{n.notificationable_id},#{n.notificationable_type}"
-  # end
+
+  ActionRecord.all.each do |a|
+    # puts a.to_json
+    puts "#{a.type} --- #{a.actionable_type}"
+  end
 end

842c428428658c82c90c250c73553e122f6a84a3
chuanjiabao <chuanjiabao1981@gmail.com>
2016-01-26 12:00:32 +0000

    QT-40 delete publications

--- engines/course_library/app/helpers/course_library/publications_helper.rb
+++
@@ -1,7 +0,0 @@
-module CourseLibrary
-  module PublicationsHelper
-    def available_customized_courses(course,teacher)
-      teacher.customized_courses - course.customized_courses
-    end
-  end
-end
--- engines/course_library/db/migrate/20160113221627_create_course_library_publications.rb
+++
@@ -1,9 +0,0 @@
-class CreateCourseLibraryPublications < ActiveRecord::Migration
-  def change
-    create_table :course_library_publications do |t|
-      t.references :course
-      t.references :courseable,polymorphic: true
-      t.timestamps null: false
-    end
-  end
-end

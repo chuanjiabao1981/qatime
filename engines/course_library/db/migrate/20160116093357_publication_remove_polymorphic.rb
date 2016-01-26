66e72e80e163d1e661f7318b22e677f34a2b6792
chuanjiabao <chuanjiabao1981@gmail.com>
2016-01-26 11:55:34 +0000

    QT-40 delete publications

--- engines/course_library/db/migrate/20160116093357_publication_remove_polymorphic.rb
+++
@@ -1,7 +0,0 @@
-class PublicationRemovePolymorphic < ActiveRecord::Migration
-  def change
-    remove_column :course_library_publications,:courseable_id
-    remove_column :course_library_publications,:courseable_type
-    add_column    :course_library_publications,:customized_course_id,:integer
-  end
-end
--- engines/course_library/db/migrate/20160118223312_add_customized_tutorial_to_publications.rb
+++
@@ -1,6 +0,0 @@
-class AddCustomizedTutorialToPublications < ActiveRecord::Migration
-  def change
-    remove_column    :course_library_publications,:customized_course_id,:integer
-    add_column       :course_library_publications,:customized_tutorial_id,:integer
-  end
-end

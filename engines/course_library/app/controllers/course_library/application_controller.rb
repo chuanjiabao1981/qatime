e29d9591671eb92bb7cff7d31e09321ae725e733
wenlong.lwl <wenlong.lwl@alibaba-inc.com>
2016-01-28 21:15:19 +0800

    update codes..

--- engines/course_library/app/controllers/course_library/application_controller.rb
+++ engines/course_library/app/controllers/course_library/application_controller.rb
@@ -1,11 +1,13 @@
 module CourseLibrary
   class ApplicationController < ::ApplicationController
-
+    layout "course_library/application"
     def unauthorized
       redirect_to main_app.root_path
     end
     def current_resource
-      @teacher = Teacher.find(params[:teacher_id])
+      if ! params[:teacher_id].nil?
+        @teacher = Teacher.find(params[:teacher_id])
+      end
     end
   end
 end

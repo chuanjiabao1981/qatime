84cd1dc4e45e15c591422cff8b7e04119a280940
wangyang0918 <Wang279594931>
2016-01-29 14:02:06 +0800

    add tests

--- test/integration/syllabus_flows_test.rb
+++ test/integration/syllabus_flows_test.rb
@@ -1,16 +1,15 @@
 require 'test_helper'
 
-module CourseLibrary
-  class SyllabusFlowsTest < LoginTestBase
-    def setup
-      super
-    end
-
-    test "syllabus index" do
-      @teacher_session.get CourseLibrary::Engine.routes.url_helpers.syllabuses_path
-      @teacher_session.assert_response :success
-    end
-
+class SyllabusFlowsTest < LoginTestBase
+  def setup
+    super
   end
+
+  test "syllabus index" do
+    @teacher_session.get course_library.syllabuses_path
+    @teacher_session.assert_response :success
+  end
+
 end
+
 

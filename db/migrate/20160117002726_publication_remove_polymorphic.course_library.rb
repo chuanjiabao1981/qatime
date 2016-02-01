902cc118142fe333d6fc48a1e9ed89611de7d2cf
chuanjiabao <chuanjiabao1981@gmail.com>
2016-02-01 06:54:38 +0000

    remove un necessary publication migaret

--- db/migrate/20160117002726_publication_remove_polymorphic.course_library.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from course_library (originally 20160116093357)
-class PublicationRemovePolymorphic < ActiveRecord::Migration
-  def change
-    remove_column :course_library_publications,:courseable_id
-    remove_column :course_library_publications,:courseable_type
-    add_column    :course_library_publications,:customized_course_id,:integer
-  end
-end

cf170ea1e860c653f76ca82651f8b0c6e1c6452f
wangyang0918 <fly_in_gis@hotmail.com>
2016-02-02 19:45:09 +0800

    update test

--- test/integration/course_library/directory_flows_test.rb
+++ test/integration/course_library/directory_flows_test.rb
@@ -14,21 +14,25 @@ class DirectoryFlowsTest < LoginTestBase
     @teacher_session.assert_select 'a[href=?]', CourseLibrary::Engine.routes.url_helpers.syllabus_directory_path(@syllabus,@chapter1),2
   end
 
-  test "directories new" do
+  test "directories new and create" do
     @teacher_session.get CourseLibrary::Engine.routes.url_helpers.new_syllabus_directory_path(@syllabus,:p => @directory)
     @teacher_session.assert_response :success
-  end
 
-  test "directories create" do
+    c = @syllabus.directories.count
     @teacher_session.post CourseLibrary::Engine.routes.url_helpers.syllabus_directories_path(@syllabus),
                           directory: {parent_id:@directory.id,title:"new directory"}
     @teacher_session.assert_response :redirect
-    assert_equal 5,@syllabus.directories.count
+    @teacher_session.assert_equal c+1,@syllabus.directories.count
   end
 
-  test "directories edit" do
-    @teacher_session.get CourseLibrary::Engine.routes.url_helpers.edit_syllabus_directory_path(@syllabus,@directory)
+  test "directories edit and update" do
+    @teacher_session.get CourseLibrary::Engine.routes.url_helpers.edit_syllabus_directory_path(@syllabus,@chapter1)
     @teacher_session.assert_response :success
+
+    @teacher_session.patch CourseLibrary::Engine.routes.url_helpers.syllabus_directory_path(@syllabus,@chapter1),
+                           directory: {title:"chapter1 new"}
+    @teacher_session.assert_response :redirect
+    @teacher_session.assert_equal 1,@syllabus.directories.where(title:"chapter1 new").count
   end
 
 
--- test/integration/course_library/syllabus_flows_test.rb
+++ test/integration/course_library/syllabus_flows_test.rb
@@ -13,16 +13,25 @@ class SyllabusFlowsTest < LoginTestBase
     @teacher_session.assert_select 'a[href=?]', CourseLibrary::Engine.routes.url_helpers.syllabus_directory_path(@syllabus,@directory),1
   end
 
-  test "syllabuses new" do
+  test "syllabuses new and create" do
     @teacher_session.get CourseLibrary::Engine.routes.url_helpers.new_teacher_syllabus_path(@teacher)
     @teacher_session.assert_response :success
-  end
 
-  test "syllabuses create" do
+    c = @teacher.syllabuses.count
     @teacher_session.post CourseLibrary::Engine.routes.url_helpers.teacher_syllabuses_path(@teacher),
                           syllabus: {title:"new syllabus",description:"description for new syllabus"}
     @teacher_session.assert_response :redirect
-    assert_equal 2,@teacher.syllabuses.count
+    @teacher_session.assert_equal c+1,@teacher.syllabuses.count
+  end
+
+  test "syllabus edit and update" do
+    @teacher_session.get CourseLibrary::Engine.routes.url_helpers.edit_teacher_syllabus_path(@teacher,@syllabus)
+    @teacher_session.assert_response :success
+
+    @teacher_session.patch CourseLibrary::Engine.routes.url_helpers.teacher_syllabus_path(@teacher,@syllabus),
+                           syllabus: {title:"new syllabus title"}
+    @teacher_session.assert_response :redirect
+    @teacher_session.assert_equal 1,@teacher.syllabuses.where(title:"new syllabus title").count
   end
 
 end

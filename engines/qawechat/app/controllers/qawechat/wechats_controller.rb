4b8caca99f0a745c7995523ea825643e0e4345be
danrtsey-wy <fly_in_gis@hotmail.com>
2016-03-17 21:54:35 +0800

    fix a bug when has so so so many customized_courses

--- engines/qawechat/app/controllers/qawechat/wechats_controller.rb
+++ engines/qawechat/app/controllers/qawechat/wechats_controller.rb
@@ -3,9 +3,8 @@ class WechatsController < ActionController::Base
   include WechatHelper
   wechat_responder
 
-  # default text responder when no other match
-  on :text do |request, content|
-    request.reply.text "Echo:#{content}"
+  on :text, with: 'help' do |request|
+    request.reply.text '请先绑定帐号，以便接收所有通知并查看，并在菜单中查看专属课程'
   end
 
   on :event, with: 'subscribe' do |request|
@@ -13,25 +12,17 @@ class WechatsController < ActionController::Base
     request.reply.text "#{nickname}您好，欢迎订阅答疑时间微信服务平台。我们将竭诚为您服务！"
   end
 
+  on :event, with: 'unsubscribe' do |request|
+    request.reply.success
+    wechat_user = WechatUser.find_by(openid: request[:FromUserName])
+    wechat_user.destroy unless wechat_user.nil?
+  end
+
   # When user click the menu button
   on :click, with: 'CUSTOM_COURSE' do |request, key|
-    request.reply.text get_custom_courses(request[:FromUserName])
-  end
-
-  # When user sent the image
-  on :image do |request|
-    request.reply.image(request[:MediaId]) # Echo the sent image to user
-  end
-
-  # When user sent the voice
-  on :voice do |request|
-    request.reply.voice(request[:MediaId]) # Echo the sent voice to user
-  end
-
-  # When user sent the video
-  on :video do |request|
-    nickname = wechat.user(request[:FromUserName])['nickname']
-    request.reply.video(request[:MediaId], title: 'Echo', description: "Got #{nickname} sent video") # Echo the sent video to user
+    get_custom_courses(request[:FromUserName]).each do |c|
+      request.reply.text c
+    end
   end
 
   # Any not match above will fail to below
@@ -44,12 +35,13 @@ class WechatsController < ActionController::Base
       usr = User.find(wechat_user.user_id)
       user = Teacher.find(wechat_user.user_id) if usr.teacher?
       user = Student.find(wechat_user.user_id) if usr.student?
-      result = "您的专属课程有：\n"
-      index = 1
+      result = []
+      result[0] = "您的专属课程有：\n"
+      index = 0
       user.customized_courses.each do |customized_course|
-	teachers = customized_course.teachers.map {|t| t.view_name}.join(",")
+        teachers = customized_course.teachers.map {|t| t.view_name}.join(",")
         text = "#{customized_course.category}-#{customized_course.subject}-#{teachers}-#{customized_course.student.view_name}"
-        result += "#{index} " + get_customized_course_href(customized_course.id, openid, text) + "\n"
+        result[index % 8] += "#{index + 1} " + get_customized_course_href(customized_course.id, openid, text) + "\n"
         index = index + 1
       end
     end

c916375b95e5efa791fb191bea3c5d8ddf0699de
Chareice <chareice@live.com>
2016-03-31 20:28:01 +0800

    add wechat js api to message reply

---
+++ app/services/media_service/voice/create_wechat_voice_message_reply.rb
@@ -0,0 +1,13 @@
+module MediaService
+  module Voice
+    class CreateWechatVoiceMessageReply
+      def initialize(media_id)
+        @media_id = media_id
+      end
+
+      def call
+        
+      end
+    end
+  end
+end
---
+++ test/services/media_service/voice/create_wechat_voice_message_reply_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+module MediaServiceTest
+  module VoiceTest
+    class CreateWechatVoiceMessageReplyTest < ActiveSupport::TestCase
+
+      test 'create message reply' do
+        media_id = 'some media_id'
+      end
+    end
+  end
+end

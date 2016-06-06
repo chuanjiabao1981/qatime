d7b3ce4b1f9c008d5a1d88d6181650854668ea9d
heart <xinshuaifeng@126.com>
2016-06-06 13:12:42 +0800

    QT-118 #start_progress 添加初始化数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# city initialize
+%w(北京 上海 广州 深圳).each do |city_name|
+  City.create(name: city_name)
+end unless City.count > 0
+
+# admin initialize
+User.create(name: 'admin',
+            email: 'admin@admin.com',
+            password: 'admin123',
+            password_confirmation: 'admin123',
+            role: 'admin') unless User.exists?
+
+# school initialize
+%w(一中 二中 三中).each do |suffix|
+  City.find_each do |city|
+    city.schools.create(name: "#{city.name}#{suffix}")
+  end
+end unless School.exists?
+
+# initialize register codes
+School.find_each do |school|
+  RegisterCode.batch_make(10.to_s, school) # ...... 不要问我为啥10.to_s 我想静静
+end unless RegisterCode.exists?

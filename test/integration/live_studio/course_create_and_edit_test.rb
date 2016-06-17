8ca31437c0c97a00b01ffe41ad41589a46d0c17a
liyang610 <897430772@qq.com>
2016-06-17 17:45:55 +0800

    QT-124 更新辅导班测试

--- test/integration/live_studio/course_create_and_edit_test.rb
+++ test/integration/live_studio/course_create_and_edit_test.rb
@@ -35,17 +35,18 @@ module LiveStudio
 
     test "manager update a course" do
       course = Course.last
+      teacher2 = ::Teacher.find(users(:teacher2).id)
 
       visit live_studio.edit_manager_course_path(course)
       fill_in :course_name, with: '测试英语辅导课程更新'
       fill_in :course_description, with: 'edit course description'
-      fill_in :course_teacher_id, with: 2
+      fill_in :course_teacher_id, with: teacher2.id
       fill_in :course_price, with: 80.0
       click_on '更新Course'
       course.reload
       assert_equal('测试英语辅导课程更新', course.name, '辅导班名称修改错误')
       assert_equal('edit course description', course.description, '辅导班描述修改错误')
-      assert_equal(2, course.teacher_id, '辅导班老师修改错误')
+      assert_equal(teacher2.id, course.teacher_id, '辅导班老师修改错误')
       assert_equal(80.0, course.price.to_f, '辅导班定价修改错误')
     end
   end
--- test/integration/live_studio/teacher_course_test.rb
+++ test/integration/live_studio/teacher_course_test.rb
@@ -43,17 +43,19 @@ module LiveStudio
     end
 
     test "teacher update a courses" do
+      teacher2 = ::Teacher.find(users(:teacher2).id)
+
       visit live_studio.edit_teacher_course_path(@course)
       fill_in :course_name, with: '测试英语辅导课程更新'
       fill_in :course_description, with: 'edit course description'
-      fill_in :course_teacher_id, with: 2
+      fill_in :course_teacher_id, with: teacher2.id
       fill_in :course_price, with: 80.0
       click_on '更新Course'
 
-      assert_equal(@course.name, '测试英语辅导课程更新', '辅导班名称修改错误')
-      assert_equal(@course.description, 'edit course description', '辅导班描述修改错误')
-      assert_equal(@course.teacher_id, 2, '辅导班老师修改错误')
-      assert_equal(@course.price.to_f, 80.0, '辅导班定价修改错误')
+      assert_equal('测试英语辅导课程更新', @course.name, '辅导班名称修改错误')
+      assert_equal('edit course description', @course.description, '辅导班描述修改错误')
+      assert_equal(teacher2.id, @course.teacher_id, '辅导班老师修改错误')
+      assert_equal(80.0, @course.price.to_f, '辅导班定价修改错误')
     end
 
   end

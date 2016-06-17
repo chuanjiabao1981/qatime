359d03553bd68f955a6cddcb541b47e27202df7e
xinshuaifeng <xinshuaifeng@126.com>
2016-06-17 14:02:13 +0800

    QT-123 购买课程状态

--- engines/live_studio/app/models/live_studio/ticket.rb
+++ engines/live_studio/app/models/live_studio/ticket.rb
@@ -3,5 +3,12 @@ module LiveStudio
     belongs_to :course
     belongs_to :student
     belongs_to :lesson
+
+    enum state: {
+           inactive: 0,
+           active: 1,
+           used: 2,
+           waste: 99
+         }
   end
 end

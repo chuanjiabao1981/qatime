b00fc2f7febd8d4783ce2345b79364d4dac108d7
liyang610 <897430772@qq.com>
2016-06-17 16:03:58 +0800

    QT-124 teacher/course 测试

--- test/integration/live_studio/student_course_test.rb
+++ test/integration/live_studio/student_course_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 module LiveStudio
-  class TeacherCourseTest < ActionDispatch::IntegrationTest
+  class StudentCourseTest < ActionDispatch::IntegrationTest
     def setup
       @routes = Engine.routes
       @headless = Headless.new
@@ -12,6 +12,7 @@ module LiveStudio
       @workstation = @manager.workstations.sample
 
       @teacher = ::Teacher.find(users(:teacher1).id)
+      @student = ::Student.find(users(:student1).id)
       @course = Course.create(
                   name: "测试英语辅导课程",
                   description: 'new course description',
@@ -19,41 +20,31 @@ module LiveStudio
                   teacher_id: @teacher.id,
                   price: 100.0
                 )
+      CoursePurchaseRecord.create(
+        student_id: @teacher,
+        course_id: @student
+      )
 
-      log_in_as(@teacher)
+      log_in_as(@student)
     end
 
     def teardown
-      logout_as(@teacher)
+      logout_as(@student)
       Capybara.use_default_driver
     end
 
     test "teacher courses index view" do
-      visit live_studio.teacher_courses_path
+      visit live_studio.student_courses_path
       page.has_content? @course.name
     end
 
     test "teacher courses show view" do
-      visit live_studio.teacher_course_path(@course)
+      visit live_studio.student_course_path(@course)
       page.has_content? @course.name
       page.has_content? @course.teacher.name
       page.has_content? @course.work_station.name
       page.has_content? @course.description
       page.has_content? @course.price
-    end
-
-    test "teacher update a courses" do
-      visit live_studio.edit_teacher_course_path(@course)
-      fill_in :course_name, with: '测试英语辅导课程更新'
-      fill_in :course_description, with: 'edit course description'
-      fill_in :course_teacher_id, with: 2
-      fill_in :course_price, with: 80.0
-      click_on '更新Course'
-
-      assert_equal(@course.name, '测试英语辅导课程更新', '辅导班名称修改错误')
-      assert_equal(@course.description, 'edit course description', '辅导班描述修改错误')
-      assert_equal(@course.teacher_id, 2, '辅导班老师修改错误')
-      assert_equal(@course.price.to_f, 80.0, '辅导班定价修改错误')
     end
 
   end

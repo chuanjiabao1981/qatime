9d225c89f10a05c41103f60f837b00db6539a7c8
liyang610 <897430772@qq.com>
2016-06-27 16:56:01 +0800

    QT-137 直播计时测试

---
+++ test/integration/live_studio/teacher_live_studio_begin_and_end_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+module LiveStudio
+  class TeacherLiveStudioBeginAndEndTest < ActionDispatch::IntegrationTest
+    def setup
+      @routes = Engine.routes
+      @headless = Headless.new
+      @headless.start
+      Capybara.current_driver = :selenium_chrome
+
+      @teacher = ::Teacher.find(users(:teacher_one).id)
+      log_in_as(@teacher)
+    end
+
+    def teardown
+      logout_as(@teacher)
+      Capybara.use_default_driver
+    end
+
+    test "teacher begin and end live_studio" do
+      course = live_studio_courses(:course_with_lesson)
+      lesson = live_studio_lessons(:lesson_three)
+
+      visit live_studio.teacher_course_path(course)
+      page.has_content? "开始直播"
+      click_on '开始直播'
+
+      visit live_studio.teacher_course_path(course)
+      page.has_content? "结束直播"
+      click_on '结束直播'
+    end
+
+  end
+end

940c76a609a571bfa1137628ebcd1ad8118c92c7
xinshuaifeng <xinshuaifeng@126.com>
2016-06-27 15:59:31 +0800

    QT-136 支付页面

--- engines/payment/app/controllers/payment/orders_controller.rb
+++ engines/payment/app/controllers/payment/orders_controller.rb
@@ -3,7 +3,7 @@ require_dependency "payment/application_controller"
 module Payment
   class OrdersController < ApplicationController
     def show
-      @order = Order.find_by!(order_no: params[:id])
+      @order = current_user.orders.find_by!(order_no: params[:id])
     end
   end
 end

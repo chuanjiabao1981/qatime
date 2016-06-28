20035114aeb55894ae394c8e034ea091353041f0
wangzc <wchtest001@163.com>
2016-06-28 16:53:57 +0800

    QT-129 修改订单状态

--- engines/payment/app/controllers/payment/orders_controller.rb
+++ engines/payment/app/controllers/payment/orders_controller.rb
@@ -34,7 +34,9 @@ module Payment
       end
       if flag
         @order = Order.find_by(order_no: @result['out_trade_no'])
-        @order.pay if @order.unpaid?
+
+        @order.pay && @order.save if @order.unpaid?
+        
         render :xml => {return_code: "SUCCESS"}.to_xml(root: 'xml', dasherize: false)
       else
         render :xml => {return_code: "FAIL", return_msg: "签名失败"}.to_xml(root: 'xml', dasherize: false)

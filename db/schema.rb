cc48d88550fb373b68d2d597b65196f4e0153d0d
liyang610 <897430772@qq.com>
2016-06-28 06:00:09 +0000

    QT-137 修改live_studio_play_records表

--- db/schema.rb
+++ db/schema.rb
@@ -474,7 +474,7 @@ ActiveRecord::Schema.define(version: 20160627065822) do
   add_index "live_studio_lessons", ["teacher_id"], name: "index_live_studio_lessons_on_teacher_id", using: :btree
 
   create_table "live_studio_play_records", force: :cascade do |t|
-    t.integer  "student_id"
+    t.integer  "user_id"
     t.integer  "course_id"
     t.integer  "lesson_id"
     t.datetime "start_time_at"
@@ -485,7 +485,7 @@ ActiveRecord::Schema.define(version: 20160627065822) do
 
   add_index "live_studio_play_records", ["course_id"], name: "index_live_studio_play_records_on_course_id", using: :btree
   add_index "live_studio_play_records", ["lesson_id"], name: "index_live_studio_play_records_on_lesson_id", using: :btree
-  add_index "live_studio_play_records", ["student_id"], name: "index_live_studio_play_records_on_student_id", using: :btree
+  add_index "live_studio_play_records", ["user_id"], name: "index_live_studio_play_records_on_user_id", using: :btree
 
   create_table "live_studio_streams", force: :cascade do |t|
     t.string   "protocol",   limit: 20

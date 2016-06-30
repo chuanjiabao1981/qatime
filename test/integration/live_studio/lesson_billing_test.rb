a5cdfe737c37a81bc640546d9db14d89f8ea137b
xinshuaifeng <xinshuaifeng@126.com>
2016-06-30 16:45:25 +0800

    QT-136 教师开始直播、完成直播、手动结算测试

---
+++ test/integration/live_studio/lesson_billing_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+module LiveStudio
+  # 课程结算
+  class LessonBillingTest < ActionDispatch::IntegrationTest
+    def setup
+      @routes = Engine.routes
+      @headless = Headless.new
+      @headless.start
+      Capybara.current_driver = :selenium_chrome
+      @teacher = users(:english_teacher)
+
+      log_in_as(@teacher)
+    end
+
+    def teardown
+      logout_as(@teacher)
+      Capybara.use_default_driver
+    end
+
+    test "teacher start live" do
+      course = live_studio_courses(:course_with_lessons)
+      lesson = live_studio_lessons(:english_lesson_today)
+      visit live_studio.teacher_course_path(course)
+      click_on("准备上课")
+      page.assert_selector("a.live")
+      lesson.reload
+      assert_equal("ready", lesson.status)
+      click_on("开始直播")
+      page.assert_selector("a.finish")
+      lesson.reload
+      assert_equal("teaching", lesson.status)
+    end
+
+    test "lesson finish and billing" do
+      course = live_studio_courses(:course_with_lessons)
+      lesson = live_studio_lessons(:english_lesson_onlive)
+      visit live_studio.teacher_course_path(course)
+      click_on("结束直播")
+      lesson.reload
+      assert_equal("completed", lesson.status)
+    end
+
+    test "lesson complete and billing" do
+      course = live_studio_courses(:course_with_lessons)
+      lesson = live_studio_lessons(:english_lesson_finished)
+      visit live_studio.teacher_course_path(course)
+      click_on("立即结算")
+      lesson.reload
+      assert_equal("completed", lesson.status)
+    end
+  end
+end

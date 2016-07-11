d91eec306c8e2a5a10016fce0aa9bb2dbe73feb4
xinshuaifeng <xinshuaifeng@126.com>
2016-07-11 11:35:43 +0800

    QT-154 错误修复

--- app/services/live_service/chat_team_manager.rb
+++ app/services/live_service/chat_team_manager.rb
@@ -6,8 +6,11 @@ module LiveService
 
     # 创建聊天群组
     # 创建群组，默认不需要用户同意、不允许任何人申请加入
-    def instance_team(course, teacher_account, members)
-      return course.chat_team if course.chat_team
+    def instance_team(course, teacher_account)
+      if course.chat_team
+        @team = course.chat_team
+        return @team
+      end
       team_id = Chat::IM.team_create(tname: "#{course.name} 讨论组", owner: teacher_account.accid, members: [], msg: "#{course.name} 讨论组")
       @team = Chat::Team.create(team_id: team_id, name: "#{@course.name} 讨论组", live_studio_course: @course)
     end
@@ -19,12 +22,13 @@ module LiveService
     end
 
     # 拉入群组
-    def add_to_team(team, members, role, remote = false)
-      current_members = team.join_records.map(&:account_id)
+    def add_to_team(members, role, remote = false)
+      members = members.to_a
+      current_members = @team.join_records.map(&:account_id)
       members.delete_if {|m| current_members.include?(m.id)}
-      Chat::IM.team_add(team.team_id, team.owner, "#{@course.name} 讨论组", members) if remote
+      Chat::IM.team_add(@team.team_id, @team.owner, "#{@course.name} 讨论组", members) if remote
       members.each do |member|
-        Chat::JoinRecord.create(team_id: team.id, account_id: member.id, role: role)
+        Chat::JoinRecord.create(team_id: @team.id, account_id: member.id, role: role)
       end
     end
   end

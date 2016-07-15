56015991965e57b2d9c590407d5c12d667ffc80b
liyang610 <897430772@qq.com>
2016-07-15 17:32:09 +0800

    QT-167 聊天室集成测试删除team手动创建

--- test/integration/chat/msgs_chat_team_test.rb
+++ test/integration/chat/msgs_chat_team_test.rb
@@ -17,22 +17,10 @@ module LiveStudio
       @channel = live_studio_channels(:three)
 
       result_data = Chat::IM.account_create(@teacher_account.accid, @teacher_account.name, @teacher.avatar_url(:small), @teacher_account.token)
-
       @teacher_account.update_columns(result_data) if result_data
 
       result_data = Chat::IM.account_create(@student_account.accid, @student_account.name, @student.avatar_url(:small), @student_account.token)
       @student_account.update_columns(result_data) if result_data
-
-      @team = ::Chat::Team.create(
-        name: "#{@course.name} 讨论组",
-        live_studio_course: @course,
-        owner: @teacher_account.accid
-      )
-
-      if @team.team_id.blank?
-        team_id = Chat::IM.team_create(tname: @team.name, owner: @team.owner, members: [@student_account.accid], msg: "#{@course.name} 讨论组")
-        @team = @team.update_column(:team_id, team_id)
-      end
     end
 
     def teardown
@@ -40,27 +28,38 @@ module LiveStudio
     end
 
     test "student chat in team" do
-      Capybara.using_session("student") do
-        log_in_as(@student)
-        visit live_studio.play_course_path(@course)
-
-        sleep(3)
-        fill_in "message-area", with: "大家好呀"
-        click_on "发送"
-        sleep(10)
-
-        page.has_content? "大家好呀"
-      end
       Capybara.using_session("teacher") do
         log_in_as(@teacher)
+        visit chat.live_studio_course_teams_finish_path(@course)
+        @course.reload
         visit live_studio.play_course_path(@course)
 
+        p Chat::IM.team_query(@course.chat_team.team_id)
+        p "----teacher_chat_account=#{@teacher_account.accid}"
+        p "----student_chat_account=#{@student_account.accid}"
+
         sleep(3)
+
         fill_in "message-area", with: "同学们，大家好呀"
         click_on "发送"
         sleep(10)
 
         page.has_content? "同学们，大家好呀"
+      end
+
+      Capybara.using_session("student") do
+        log_in_as(@student)
+
+        visit live_studio.play_course_path(@course)
+
+        sleep(3)
+        page.has_content? "同学们，大家好呀"
+
+        fill_in "message-area", with: "大家好呀"
+        click_on "发送"
+        sleep(100)
+
+        page.has_content? "大家好呀"
       end
 
       Capybara.using_session("student") do

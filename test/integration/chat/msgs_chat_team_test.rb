7cee80d3852534b816cf578ef880a6403e74e33b
liyang610 <897430772@qq.com>
2016-07-15 19:52:33 +0800

    QT-167 修改chat的集成测试

--- test/integration/chat/msgs_chat_team_test.rb
+++ test/integration/chat/msgs_chat_team_test.rb
@@ -10,11 +10,7 @@ module LiveStudio
 
       @teacher = users(:teacher_with_chat_account)
       @student = users(:student_tally)
-      @teacher_account = @teacher.chat_account
-      @student_account = @student.chat_account
-
       @course = live_studio_courses(:course_with_junior_teacher)
-      @channel = live_studio_channels(:three)
 
       LiveService::ChatAccountFromUser.new(@teacher).instance_account(true)
       LiveService::ChatAccountFromUser.new(@student).instance_account(true)
@@ -34,13 +30,31 @@ module LiveStudio
 
         fill_in "message-area", with: "同学们，大家好呀"
         click_on "发送"
-        sleep(30)
+        sleep(10)
 
         page.has_content? "同学们，大家好呀"
       end
 
-      Capybara.using_session("teacher") do
+      Capybara.using_session("student") do
+        log_in_as(@student)
+
+        visit live_studio.play_course_path(@course)
+
+        sleep(3)
+        page.has_content? "同学们，大家好呀"
+
+        fill_in "message-area", with: "大家好呀"
+        click_on "发送"
+        sleep(10)
+
+        page.has_content? "大家好呀"
+      end
+
+      Capybara.using_session("student") do
         logout_as(@student)
+      end
+      Capybara.using_session("teacher") do
+        logout_as(@teacher)
       end
     end
   end

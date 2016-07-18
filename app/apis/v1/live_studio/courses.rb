1f163c73e78154776d622b6846bfe604fca18f96
liyang610 <897430772@qq.com>
2016-07-18 17:27:07 +0800

    QT-183 修改辅导班查询接口路由

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -2,17 +2,17 @@ module V1
   # 系统环境
   module LiveStudio
     class Courses < Grape::API
-      namespace :live_studio do
+      namespace "live_studio/teacher", path: "live_studio" do
         resource :courses do
           before do
             @current_user ||= ::Teacher.last
             ATTRIBUTES_ARR = [:name, :subject, :status, :description, :lesson_count, :preset_lesson_count, :completed_lesson_count].freeze
           end
 
-          desc '辅导班列表'
+          desc '辅导班列表接口'
           params do
           end
-          get :courses do
+          get do
             courses_data = @current_user.live_studio_courses.map do |course|
               {}.tap do |h|
                 ATTRIBUTES_ARR.each do |attri|
@@ -24,21 +24,31 @@ module V1
             { info: { courses: courses_data } }
           end
 
-          desc '辅导班详情'
+          desc '辅导班全信息接口'
           params do
-            requires :id, type: Integer, desc: '辅导班ID'
           end
-          get :course do
-            course = @current_user.live_studio_courses.find(params[:id])
+          get :full do
+            courses_data = @current_user.live_studio_courses
 
-            course_data = {}.tap do |h|
-              ATTRIBUTES_ARR.each do |attri|
-                h[attri] = course.send attri
-              end
+            { info: { courses: courses_data } }
+          end
+
+          route_param :id do
+            desc '辅导班详情接口'
+            params do
+              requires :id, type: Integer, desc: '辅导班ID'
             end
+            get do
+              course = @current_user.live_studio_courses.find(params[:id])
+              course_data = {}.tap do |h|
+                ATTRIBUTES_ARR.each do |attri|
+                  h[attri] = course.send attri
+                end
+              end
 
-            { info: { course: course_data } }
-          end
+              { info: { course: course_data } }
+            end
+        end
         end
       end
     end

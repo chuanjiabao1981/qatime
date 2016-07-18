5339e7f27d8c0d5d6d14eeb5542c10bfe4508d54
liyang610 <897430772@qq.com>
2016-07-18 15:58:37 +0800

    QT-183 辅导班详情接口

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -3,25 +3,41 @@ module V1
   module LiveStudio
     class Courses < Grape::API
       namespace :live_studio do
-        helpers do
-          def current_user
-            @current_user ||= ::Teacher.last
-          end
-        end
-
         resource :courses do
-          desc '辅导班列表.'
+          before do
+            @current_user ||= ::Teacher.last
+            ATTRIBUTES_ARR = [:name, :subject, :status, :description, :lesson_count, :preset_lesson_count, :completed_lesson_count].freeze
+          end
+
+          desc '辅导班列表'
           params do
           end
           get :courses do
-            courses = current_user.live_studio_courses.map do |course|
+            courses_data = @current_user.live_studio_courses.map do |course|
               {}.tap do |h|
-                [:name, :description, :price, :lesson_count, :preset_lesson_count, :completed_lesson_count].each do |attri|
+                ATTRIBUTES_ARR.each do |attri|
                   h[attri] = course.send attri
                 end
               end
             end
-            { info: { courses: courses } }
+
+            { info: { courses: courses_data } }
+          end
+
+          desc '辅导班详情'
+          params do
+            requires :id, type: Integer, desc: '辅导班ID'
+          end
+          get :course do
+            course = @current_user.live_studio_courses.find(params[:id])
+
+            course_data = {}.tap do |h|
+              ATTRIBUTES_ARR.each do |attri|
+                h[attri] = course.send attri
+              end
+            end
+
+            { info: { course: course_data } }
           end
         end
       end

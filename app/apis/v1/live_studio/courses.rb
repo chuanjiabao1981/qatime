22a7d313dba5241d7426394ff39923120274d3ce
liyang610 <897430772@qq.com>
2016-07-18 17:33:01 +0800

    QT-183 修改辅导班详情接口

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -32,23 +32,21 @@ module V1
 
             { info: { courses: courses_data } }
           end
-
-          route_param :id do
-            desc '辅导班详情接口'
-            params do
-              requires :id, type: Integer, desc: '辅导班ID'
-            end
-            get do
-              course = @current_user.live_studio_courses.find(params[:id])
-              course_data = {}.tap do |h|
-                ATTRIBUTES_ARR.each do |attri|
-                  h[attri] = course.send attri
-                end
-              end
-
-              { info: { course: course_data } }
-            end
         end
+
+        desc '辅导班详情接口'
+        params do
+          requires :id, type: Integer, desc: '辅导班ID'
+        end
+        get 'courses/:id' do
+          course = @current_user.live_studio_courses.find(params[:id])
+          course_data = {}.tap do |h|
+            ATTRIBUTES_ARR.each do |attri|
+              h[attri] = course.send attri
+            end
+          end
+
+          { info: { course: course_data } }
         end
       end
     end

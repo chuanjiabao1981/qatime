6f27b46ad709053256eb6d138a45d23d954badea
liyang610 <897430772@qq.com>
2016-07-18 14:15:16 +0800

    QT-183 辅导班详情接口

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -1,13 +1,27 @@
 module V1
   # 系统环境
-  module LiveStudio 
+  module LiveStudio
     class Courses < Grape::API
       namespace :live_studio do
+        helpers do
+          def current_user
+            @current_user ||= ::Teacher.last
+          end
+        end
+
         resource :courses do
           desc '辅导班列表.'
           params do
           end
-          get do
+          get :courses do
+            courses = current_user.live_studio_courses.map do |course|
+              {}.tap do |h|
+                [:name, :description, :price, :lesson_count, :preset_lesson_count, :completed_lesson_count].each do |attri|
+                  h[attri] = course.send attri
+                end
+              end
+            end
+            { info: { courses: courses } }
           end
         end
       end

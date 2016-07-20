f77d415d6600ca81f55736a1d1b2cb89a8c332d1
liyang610 <897430772@qq.com>
2016-07-20 10:53:16 +0800

    QT-177 添加用户登出接口

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,17 +8,25 @@ module SessionsHelper
       cookies.permanent[:remember_user_type]  = user.role
     end
 
+    client_type = LoginToken.client_types[client_type]
     login_token = user.login_tokens.find_or_create_by(client_type: client_type)
     login_token.update_attribute(:remember_token, User.digest(remember_token))
 
     current_user = user
   end
 
-  def sign_out(client_type = :web)
+  def sign_out
+    client_type = headers['Client-Type'].to_sym || :web
     if client_type == :web
+      current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
       cookies.delete(:remember_token)
       cookies.delete(:remember_user_type)
+      login_token_object = current_user
+    else
+      client_type = LoginToken.client_types[client_type]
+      login_token_object = current_user.login_tokens.where(client_type: client_type).first
     end
+    login_token_object.update_attribute(:remember_token, User.digest(User.new_remember_token))
     self.current_user = nil
   end
 
@@ -39,7 +47,6 @@ module SessionsHelper
 
   def user_from_remember_token
     digest_token = headers['Remember-Token'] || User.digest(cookies[:remember_token])
-
     LoginToken.find_by(remember_token: digest_token).user
   end
 

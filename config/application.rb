fb87c49c7677bb88e758d58645bfaa9ea3a34484
xinshuaifeng <xinshuaifeng@126.com>
2016-07-20 14:32:30 +0800

    QT-177 自动加载路径

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Qatime
     config.active_job.queue_adapter = :sidekiq
 
 
-    config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
+    # config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
     # config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
 
   end

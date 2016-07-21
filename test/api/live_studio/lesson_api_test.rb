de26e96ca2df20555f6763911096d98b09f26ae0
Luke <wchtest001@163.com>
2016-07-21 10:26:11 +0800

    QT-178 修改接口&测试

--- test/api/live_studio/lesson_api_test.rb
+++ test/api/live_studio/lesson_api_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
+class Qatime::LessonAPITest < ActionDispatch::IntegrationTest
   def app
     Rails.application
   end
@@ -8,20 +8,27 @@ class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
     JSON.parse(response.body)['data']
   end
 
+  def get_url(url, params)
+    teacher = users(:teacher1)
+    token = teacher.login_tokens.first.remember_token
+    url['teacher_id'] = teacher.id.to_s
+    get url, params, {'Remember-Token' => token }
+  end
+
   test 'GET /api/v1/live_studio/teacher/live_start' do
     lesson = live_studio_lessons(:ready_lesson_today)
-    get '/api/v1/live_studio/teacher/live_start?lesson_id=%d' % lesson.id
+    get_url("/api/v1/live_studio/teachers/teacher_id/live_start",{lesson_id: lesson.id})
     assert_response :success
     assert data.length == 32
   end
 
   test 'GET /api/v1/live_studio/teacher/heartbeat' do
     lesson = live_studio_lessons(:english_lesson_onlive)
-    get '/api/v1/live_studio/teacher/heartbeat?lesson_id=%d' % lesson.id
+    get_url('/api/v1/live_studio/teachers/teacher_id/heartbeat', {lesson_id: lesson.id})
     assert_response :success
     assert data.length == 32, '没有正确返回token'
     token = data
-    get '/api/v1/live_studio/teacher/heartbeat?lesson_id=%d&token=%s' % [lesson.id, token]
+    get_url('/api/v1/live_studio/teachers/teacher_id/heartbeat', {lesson_id: lesson.id, token: token})
     assert_response :success
     assert data.length == 32, '没有正确返回token'
     assert data == token, '应该和之前的token一致'
@@ -29,7 +36,7 @@ class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
 
   test 'GET //api/v1/live_studio/teacher/live_end' do
     lesson = live_studio_lessons(:english_lesson_onlive)
-    get '/api/v1/live_studio/teacher/live_end?lesson_id=%d' % lesson.id
+    get_url('/api/v1/live_studio/teachers/teacher_id/live_end', {lesson_id: lesson.id})
     assert_response :success
     assert data, '没有正确返回true'
   end

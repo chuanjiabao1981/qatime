e8cb24d7f566f01881aec4ee08a218a48a682a7a
liyang610 <897430772@qq.com>
2016-07-21 09:38:20 +0800

    QT-177 修改登录接口测试

--- test/api/sessions_api_test.rb
+++ test/api/sessions_api_test.rb
@@ -12,12 +12,12 @@ class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
 
     post '/api/v1/sessions', {
       email: teacher.email,
-      password: teacher.password
-    },{ 'Client-Type': 'pc'}
+      password: teacher.password,
+      client_type: 'pc'
+    }
 
     assert_response :success
     res = JSON.parse(response.body)
-
     assert_equal 1, res['status'], '状态码不对'
     assert_equal teacher.login_tokens.where(client_type: LoginToken.client_types[:pc]).first.remember_token, res['data']['remember_token'], '状态码不对'
   end

756d6709cac66c3c12000e5647e2f0bb25a813e9
liyang610 <897430772@qq.com>
2016-07-22 10:05:15 +0800

    QT-176 添加历史消息测试

---
+++ test/integration/chat/history_msgs_chat_team_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+
+module LiveStudio
+  class HistoryMsgsChatTeamTest < ActionDispatch::IntegrationTest
+    def setup
+      @routes = Engine.routes
+      @headless = Headless.new
+      @headless.start
+      Capybara.current_driver = :selenium_chrome
+
+      @teacher = users(:teacher_with_chat_account)
+      @student = users(:student_tally)
+      @course = live_studio_courses(:course_with_junior_teacher)
+
+      LiveService::ChatAccountFromUser.new(@teacher).instance_account(true)
+      LiveService::ChatAccountFromUser.new(@student).instance_account(true)
+    end
+
+    def teardown
+      Capybara.use_default_driver
+    end
+
+    test "student get histroy chat in team" do
+      Capybara.using_session("teacher") do
+        log_in_as(@teacher)
+        @course.reload
+        visit chat.live_studio_course_teams_finish_path(@course)
+        @course.reload
+        visit live_studio.play_course_path(@course)
+
+        fill_in "message-area", with: "同学们，大家好呀"
+        click_on "发送"
+        fill_in "message-area", with: "同学们，大家好呀2"
+        click_on "发送"
+      end
+
+      Capybara.using_session("student") do
+        log_in_as(@student)
+        visit live_studio.play_course_path(@course)
+
+        sleep(5)
+        click_on "历史消息"
+
+        page.has_selector?('div#history-area')
+        page.find('div#history-area').has_content?('同学们，大家好呀')
+        page.find('div#history-area').has_content?('同学们，大家好呀2')
+      end
+
+      Capybara.using_session("student") do
+        logout_as(@student, true)
+      end
+      Capybara.using_session("teacher") do
+        logout_as(@teacher, true)
+      end
+    end
+  end
+end

31e27c6ebc6c5471e65488a355c98e697049ce5c
wangzc <wchtest001@163.com>
2016-07-26 19:44:40 +0800

    QT-191 测试修改

--- test/integration/live_studio/lesson_status_change_test.rb
+++ test/integration/live_studio/lesson_status_change_test.rb
@@ -6,42 +6,42 @@ class LiveStudio::TeacherLessonTest < ActionDispatch::IntegrationTest
     @course = live_studio_courses(:tasting_course)
   end
 
-  # test 'new lesson status ready or init' do
-  #   ready_lesson = @course.lessons.create(
-  #       name: 'test ready',
-  #       description: 'test desc',
-  #       start_time: '12:00',
-  #       end_time: '12:30',
-  #       class_date: Date.today
-  #   )
-  #   assert ready_lesson.ready?, 'class_date为今天应该为ready状态'
-  #   init_lesson = @course.lessons.create(
-  #       name: 'test ready',
-  #       description: 'test desc',
-  #       start_time: '12:00',
-  #       end_time: '12:30',
-  #       class_date: Date.tomorrow
-  #   )
-  #   assert init_lesson.init? ,'class_date为明天应该为init状态'
-  # end
+  test 'new lesson status ready or init' do
+    ready_lesson = @course.lessons.create(
+        name: 'test ready',
+        description: 'test desc',
+        start_time: '12:00',
+        end_time: '12:30',
+        class_date: Date.today
+    )
+    assert ready_lesson.ready?, 'class_date为今天应该为ready状态'
+    init_lesson = @course.lessons.create(
+        name: 'test ready',
+        description: 'test desc',
+        start_time: '12:00',
+        end_time: '12:30',
+        class_date: Date.tomorrow
+    )
+    assert init_lesson.init? ,'class_date为明天应该为init状态'
+  end
 
-  # test 'service lesson start course teaching?' do
-  #   preview_course = live_studio_courses(:course_preview)
-  #   ready_lesson = live_studio_lessons(:ready_lesson_today2)
-  #   LiveService::LessonDirector.new(ready_lesson).lesson_start
-  #   preview_course.reload
-  #   ready_lesson.reload
-  #   assert preview_course.teaching? ,'辅导班应改为上课中'
-  #   assert ready_lesson.live_start_at.present?, '课程直播开始时间为空'
-  #   assert preview_course.lessons.waiting_finish.blank?, 'closed paused 的数据不为空'
-  #   assert ready_lesson.live_sessions.present?, '直播中的课程应该有心跳记录'
-  # end
-  #
-  # test 'service lesson start false' do
-  #   false_lesson = live_studio_lessons(:ready_lesson_for_false)
-  #   flag = LiveService::LessonDirector.new(false_lesson).lesson_start
-  #   assert !flag, '已有直播中的辅导班开始直播课程会返回false'
-  # end
+  test 'service lesson start course teaching?' do
+    preview_course = live_studio_courses(:course_preview)
+    ready_lesson = live_studio_lessons(:ready_lesson_today2)
+    LiveService::LessonDirector.new(ready_lesson).lesson_start
+    preview_course.reload
+    ready_lesson.reload
+    assert preview_course.teaching? ,'辅导班应改为上课中'
+    assert ready_lesson.live_start_at.present?, '课程直播开始时间为空'
+    assert preview_course.lessons.waiting_finish.blank?, 'closed paused 的数据不为空'
+    assert ready_lesson.live_sessions.present?, '直播中的课程应该有心跳记录'
+  end
+
+  test 'service lesson start false' do
+    false_lesson = live_studio_lessons(:ready_lesson_for_false)
+    flag = LiveService::LessonDirector.new(false_lesson).lesson_start
+    assert !flag, '已有直播中的辅导班开始直播课程会返回false'
+  end
 
   test 'service ready_today_lessons status change' do
     LiveService::LessonDirector.ready_today_lessons

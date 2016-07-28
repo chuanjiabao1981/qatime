2ce96d6813ac1bf1b40a3e1c41ffeb42d6cf0459
liyang610 <897430772@qq.com>
2016-07-28 18:54:15 +0800

    QT-194 修改sidebar测试

--- test/integration/student_view_top_and_side_bar_test.rb
+++ test/integration/student_view_top_and_side_bar_test.rb
@@ -6,7 +6,7 @@ class StudentViewTopAndSideBarTest < ActionDispatch::IntegrationTest
     @headless.start
     Capybara.current_driver = :selenium_chrome
 
-    @student = users(:student_tally)
+    @student = users(:student1)
     log_in_as(@student)
   end
 
@@ -16,6 +16,7 @@ class StudentViewTopAndSideBarTest < ActionDispatch::IntegrationTest
   end
 
   test "student view top bar" do
+    visit info_student_path(@student)
     assert page.has_content?('首页'), 'topbar首页不存在'
     assert page.has_content?('公开课程'), 'topbar公开课程不存在'
     assert page.has_content?('专属课程'), 'topbar专属课程不存在'
@@ -26,6 +27,7 @@ class StudentViewTopAndSideBarTest < ActionDispatch::IntegrationTest
   end
 
   test "student view side bar" do
+    visit info_student_path(@student)
     assert page.has_content?('用户设置'), 'sidebar用户设置不存在'
     assert page.has_content?('我的订单'), 'sidebar我的订单不存在'
     assert page.has_content?('我的辅导'), 'sidebar我的辅导不存在'

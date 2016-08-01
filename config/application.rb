351144e3b0d2d9406632e57c8db72204a8be88a2
liyang610 <897430772@qq.com>
2016-08-01 15:54:47 +0800

    QT-198 student新页面i18n

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,9 @@ module Qatime
     config.time_zone = 'Beijing'
     I18n.enforce_available_locales = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     config.i18n.default_locale = :cn
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.logger = false

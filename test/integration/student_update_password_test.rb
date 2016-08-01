47040c7109ec86ea402899765a75609bf1813fdf
liyang610 <897430772@qq.com>
2016-08-01 21:19:02 +0800

    QT-205 修改student修改密码测试

--- test/integration/student_update_password_test.rb
+++ test/integration/student_update_password_test.rb
@@ -5,18 +5,16 @@ class StudentUpdatePasswordTest < ActionDispatch::IntegrationTest
     @headless = Headless.new
     @headless.start
     Capybara.current_driver = :selenium_chrome
-
-    @student = users(:student1)
-    log_in_as(@student)
   end
 
   def teardown
-    new_logout_as(@student)
     Capybara.use_default_driver
   end
 
   test "student update password" do
-    visit security_setting_student_path(@student)
+    student = users(:student1)
+    log_in_as(student)
+    visit security_setting_student_path(student)
 
     click_on "修改登录密码", match: :first
 
@@ -24,9 +22,22 @@ class StudentUpdatePasswordTest < ActionDispatch::IntegrationTest
     fill_in :student_password, with: 'pa123456'
     fill_in :student_password_confirmation, with: 'pa123456'
     click_on "保存", match: :first
+    student.reload
+    assert_equal(true, student.authenticate('pa123456').present?, '更新密码错误')
+    new_logout_as(student)
+  end
 
-    @student.reload
+  test "student update password new password too short" do
+    student = users(:student_one_with_course)
+    log_in_as(student)
+    visit security_setting_student_path(student)
+    click_on "修改登录密码", match: :first
 
-    assert_equal(true, @student.authenticate('pa123456').present?, '更新密码错误')
+    fill_in :student_current_password, with: 'password'
+    fill_in :student_password, with: 'pa'
+    fill_in :student_password_confirmation, with: 'pa'
+    click_on "保存", match: :first
+
+    assert page.has_content? "过短（最短为 6 个字符）"
   end
 end

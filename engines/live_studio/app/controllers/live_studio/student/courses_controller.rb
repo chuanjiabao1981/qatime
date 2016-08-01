f9fe2c4e90059a0e0225dfab218c2e40461264ec
wangzc <wchtest001@163.com>
2016-08-01 14:18:25 +0800

    QT-200 国际化&部分文件格式化

--- engines/live_studio/app/controllers/live_studio/student/courses_controller.rb
+++ engines/live_studio/app/controllers/live_studio/student/courses_controller.rb
@@ -3,28 +3,7 @@ require_dependency "live_studio/student/base_controller"
 module LiveStudio
   class Student::CoursesController < Student::BaseController
     def index
-
-      @tickets = @student.live_studio_tickets.visiable.joins('left join live_studio_courses on live_studio_tickets.course_id = live_studio_courses.id').
-          includes(course: :teacher).includes(course: :lessons)
-      @tickets =
-          case params[:filter]
-            when 'today'
-              @tickets.select do |ticket|
-                ticket.course.lessons.select do |lesson|
-                  lesson.class_date == Date.today
-                end.present?
-              end
-            when 'waiting'
-              tickets(:preview)
-            when 'teaching'
-              tickets(:teaching)
-            when 'closed'
-              tickets(:completed)
-            when 'tasted'
-              @student.live_studio_taste_tickets.includes(course: :teacher).includes(course: :lessons)
-            else
-              @tickets
-          end
+      @tickets = LiveService::LiveService.tickets_for_cate(@student, params[:cate])
       @tickets = @tickets.paginate(page: params[:per_page])
     end
 
@@ -34,11 +13,6 @@ module LiveStudio
       @lessons = @course.lessons.order(:id).paginate(page: params[:page])
       @play_records = PlayRecord.where(user_id: @student.id, lesson_id: @lessons.map(&:id))
       @play_hash = @play_records.inject({}){ |hash, v| hash[v.lesson_id] = v.id; hash }
-    end
-
-    private
-    def tickets(course_status)
-      @tickets.where('live_studio_courses.status = ?', LiveStudio::Course.statuses[course_status])
     end
   end
 end

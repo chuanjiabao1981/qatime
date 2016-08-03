3dd058dddf1d62e84e770a2b30534a852147d576
lukegit <wccxiaoan@gmail.com>
2016-08-03 09:24:05 +0800

    QT-228 删除旧的直播接口

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -57,56 +57,6 @@ module V1
               end
             end
 
-
-            desc '直播开始接口' do
-              headers 'Remember-Token' => {
-                  description: 'RememberToken',
-                  required: true
-              }
-            end
-            params do
-              requires :lesson_id, type: Integer, desc: '课程ID'
-            end
-            get :live_start do
-              @lesson = ::LiveStudio::Lesson.find(params[:lesson_id])
-              raise_change_error_for(@lesson.ready? || @lesson.paused? || @lesson.closed?)
-              LiveService::LessonDirector.new(@lesson).lesson_start
-              @lesson.current_live_session.token
-              present @lesson, with: Entities::LiveStudio::Lesson, type: :live_start
-            end
-
-            desc '直播心跳通知接口' do
-              headers 'Remember-Token' => {
-                  description: 'RememberToken',
-                  required: true
-              }
-            end
-            params do
-              requires :lesson_id, type: Integer, desc: '课程ID'
-              optional :token, type: String, desc: '心跳token'
-            end
-            get :heartbeat do
-              @lesson = ::LiveStudio::Lesson.find(params[:lesson_id])
-              raise_change_error_for(@lesson.teaching? || @lesson.paused?)
-              @lesson.heartbeats(params[:token])
-              present @lesson, with: Entities::LiveStudio::Lesson, type: :live_start
-            end
-
-            desc '直播结束接口' do
-              headers 'Remember-Token' => {
-                  description: 'RememberToken',
-                  required: true
-              }
-            end
-            params do
-              requires :lesson_id, type: Integer, desc: '课程ID'
-            end
-            get :live_end do
-              @lesson = ::LiveStudio::Lesson.find(params[:lesson_id])
-              raise_change_error_for(@lesson.teaching? || @lesson.paused?)
-              @lesson.close!
-              present @lesson, with: Entities::LiveStudio::Lesson, type: :live_start
-            end
           end
         end
       end

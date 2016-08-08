4eb52bbdf13078157e14d4123a3e2f271002c235
liyang610 <897430772@qq.com>
2016-08-08 22:20:36 +0800

    QT-251 修改cities表

---
+++ db/migrate/20160808141858_add_province_id_to_cities.rb
@@ -0,0 +1,4 @@
+class AddProvinceIdToCities < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804022749) do
+ActiveRecord::Schema.define(version: 20160808141858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,21 +61,19 @@ ActiveRecord::Schema.define(version: 20160804022749) do
 
   create_table "chat_accounts", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "accid",      limit: 32
-    t.string   "token",      limit: 32
-    t.string   "name",       limit: 128
+    t.string   "accid"
+    t.string   "token"
+    t.string   "name"
     t.string   "icon"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "chat_accounts", ["user_id"], name: "chat_accounts_user_id_unique", unique: true, using: :btree
   add_index "chat_accounts", ["user_id"], name: "index_chat_accounts_on_user_id", using: :btree
 
   create_table "chat_join_records", force: :cascade do |t|
     t.integer  "account_id"
     t.integer  "team_id"
-    t.string   "nick_name",  limit: 64
     t.string   "role",       limit: 16
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
@@ -94,7 +92,6 @@ ActiveRecord::Schema.define(version: 20160804022749) do
     t.datetime "updated_at",                       null: false
   end
 
-  add_index "chat_teams", ["live_studio_course_id"], name: "chat_teams_course_id_unique", unique: true, using: :btree
   add_index "chat_teams", ["live_studio_course_id"], name: "index_chat_teams_on_live_studio_course_id", using: :btree
 
   create_table "cities", force: :cascade do |t|
@@ -461,8 +458,8 @@ ActiveRecord::Schema.define(version: 20160804022749) do
     t.integer  "workstation_id",                                                           null: false
     t.integer  "status",                                                     default: 0
     t.text     "description"
-    t.decimal  "price",                              precision: 8, scale: 2, default: 0.0
-    t.decimal  "lesson_price",                       precision: 8, scale: 2, default: 0.0
+    t.decimal  "price",                              precision: 6, scale: 2, default: 0.0
+    t.decimal  "lesson_price",                       precision: 6, scale: 2, default: 0.0
     t.integer  "teacher_percentage",                                         default: 0
     t.integer  "lesson_count",                                               default: 0
     t.integer  "preset_lesson_count",                                        default: 0
@@ -942,13 +939,14 @@ ActiveRecord::Schema.define(version: 20160804022749) do
     t.string   "parent_phone"
     t.integer  "workstation_id"
     t.string   "type",                          limit: 100
+    t.text     "description"
+    t.string   "highest_education"
+    t.integer  "teaching_years"
+    t.string   "grade_range"
     t.integer  "province_id"
     t.integer  "city_id"
     t.integer  "gender",                                    default: 0
     t.date     "birthday"
-    t.string   "highest_education"
-    t.integer  "teaching_years"
-    t.string   "grade_range"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

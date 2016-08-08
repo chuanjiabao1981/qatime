28678fa2f8ded28156fe37ede1e1c61864efd8eb
xinshuaifeng <xinshuaifeng@126.com>
2016-08-08 17:12:44 +0800

    语法错误

--- engines/live_studio/app/models/live_studio/course.rb
+++ engines/live_studio/app/models/live_studio/course.rb
@@ -32,7 +32,7 @@ module LiveStudio
     has_many :tickets       # 听课证
     has_many :buy_tickets   # 普通听课证
     has_many :taste_tickets # 试听证
-    has_many :lessons, -> { order('id desc') # 课时
+    has_many :lessons, -> { order('id desc') } # 课时
 
     has_many :students, through: :buy_tickets
 

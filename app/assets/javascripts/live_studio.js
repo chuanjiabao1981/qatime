ceadd16242c28bc9188cc26ac865fc8a0dea18a6
xinshuaifeng <xinshuaifeng@126.com>
2016-08-09 18:46:45 +0800

    QT-236 观看直播id不兼容

--- app/assets/javascripts/live_studio.js
+++ app/assets/javascripts/live_studio.js
@@ -16,16 +16,12 @@
 //= require bootstrap
 //= require summernote
 //= require jquery.form
-//= require video-js
-//= require jwplayer/jwplayer
-//= require jwplayer/jwpsrv_frq
-//= require jwplayer/jwpsrv
 //= require jquery.menu-aim
 //= require chosen-jquery
 //= require canvas-to-blob.min
 //= require fancybox
 //= require_tree .
-//= require course_library/application
 //= require editable/bootstrap-editable
 //= require editable/rails
 //= require jquery.qqFace.js
+//= require live_studio/application

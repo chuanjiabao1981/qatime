c62f81d20265dc9836808336c75c7d886042861f
xinshuaifeng <xinshuaifeng@126.com>
2016-08-10 10:43:58 +0800

    QT-259 配置备份

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -13,6 +13,7 @@ namespace :nginx do
   task :replace_config do
     on roles(:web) do |host|
       puts "#{host} replace nginx config"
+      execute "sudo cp /etc/nginx/sites-enabled/qatime.nginx.conf /etc/nginx/sites-enabled/qatime-#{Date.today}.nginx.conf"
       execute "sudo cp ~/nginx.conf /etc/nginx/sites-enabled/qatime.nginx.conf"
       execute "sudo service nginx restart"
     end

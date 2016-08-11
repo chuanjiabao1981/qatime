bc430f604acf10c89c83523e9244b9eb3c70b734
liyang610 <897430772@qq.com>
2016-08-11 21:02:25 +0800

    QT-240 修改基础信息接口

--- app/apis/v1/app_constants.rb
+++ app/apis/v1/app_constants.rb
@@ -2,16 +2,7 @@ module V1
   # 基础数据获取接口
   class AppConstants < Base
     namespace :app_constant do
-      before do
-        authenticate!
-      end
-
-      desc '获取基础信息' do
-        headers 'Remember-Token' => {
-          description: 'RememberToken',
-          required: true
-        }
-      end
+      desc '获取基础信息'
       get do
         provinces = ::Province.all
         cities = ::City.all
@@ -23,44 +14,24 @@ module V1
         present :schools, schools, with: Entities::School
       end
 
-      desc '获取年级列表' do
-        headers 'Remember-Token' => {
-          description: 'RememberToken',
-          required: true
-        }
-      end
+      desc '获取年级列表'
       get :grades do
         { grades: APP_CONSTANT["grades_in_menu"] }
       end
 
-      desc '获取省份列表' do
-        headers 'Remember-Token' => {
-          description: 'RememberToken',
-          required: true
-        }
-      end
+      desc '获取省份列表'
       get :provinces do
         provinces = ::Province.all
         present provinces, with: Entities::Province
       end
 
-      desc '获取城市列表' do
-        headers 'Remember-Token' => {
-          description: 'RememberToken',
-          required: true
-        }
-      end
+      desc '获取城市列表'
       get :cities do
         cities = ::City.all
         present cities, with: Entities::City
       end
 
-      desc '获取学校列表' do
-        headers 'Remember-Token' => {
-          description: 'RememberToken',
-          required: true
-        }
-      end
+      desc '获取学校列表'
       get :schools do
         schools = ::School.all
         present schools, with: Entities::School

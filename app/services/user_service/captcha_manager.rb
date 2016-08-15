40117d4fec9e6815e11c71393cc8a3b9449e2df0
liyang610 <897430772@qq.com>
2016-08-15 19:56:01 +0800

    QT-234 修改发送验证码

--- app/services/user_service/captcha_manager.rb
+++ app/services/user_service/captcha_manager.rb
@@ -3,9 +3,12 @@ module UserService
     def self.instance_and_notice(type, send_to)
       code = ::Util.random_code
       Rails.logger.debug("The Code Is: #{code}") unless Rails.env.production?
-      SmsWorker.perform_async(SmsWorker::SEND_CAPTCHA, mobile: send_to, captcha: code) if 'mobile' == type
-      # TODO
-      EmailWorker.perform_async(EmailWorker::CHANGE_EMAIL_CAPTCHA, email: send_to, code: code) if 'email' == type
+
+      unless Rails.env.testing?
+        SmsWorker.perform_async(SmsWorker::SEND_CAPTCHA, mobile: send_to, captcha: code) if 'mobile' == type
+        EmailWorker.perform_async(EmailWorker::CHANGE_EMAIL_CAPTCHA, email: send_to, code: code) if 'email' == type
+      end
+
       code
     end
 

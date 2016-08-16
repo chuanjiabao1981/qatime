4a8812cc60bcaa269a64e84d299fd4fb79fbb61d
liyang610 <897430772@qq.com>
2016-08-16 18:03:39 +0800

    QT-234 发送邮箱验证码

--- app/services/user_service/captcha_manager.rb
+++ app/services/user_service/captcha_manager.rb
@@ -4,13 +4,13 @@ module UserService
       code = ::Util.random_code
       Rails.logger.debug("The Code Is: #{code}") unless Rails.env.production?
 
-      if type == "register"
-        SmsWorker.perform_async(SmsWorker::REGISTER_CAPTCHA, mobile: send_to, captcha: code) if 'mobile' == send_type
-      else
-        SmsWorker.perform_async(SmsWorker::SEND_CAPTCHA, mobile: send_to, captcha: code, type: type) if 'mobile' == send_type
-      end
+      # if type == "register"
+      #   SmsWorker.perform_async(SmsWorker::REGISTER_CAPTCHA, mobile: send_to, captcha: code) if 'mobile' == send_type
+      # else
+      #   SmsWorker.perform_async(SmsWorker::SEND_CAPTCHA, mobile: send_to, captcha: code, type: type) if 'mobile' == send_type
+      # end
 
-      EmailWorker.perform_async(EmailWorker::CHANGE_EMAIL_CAPTCHA, email: send_to, code: code, type: type) if 'email' == send_type
+      EmailWorker.perform_async(EmailWorker::CHANGE_EMAIL_CAPTCHA, email: send_to, code: code) if 'email' == send_type
       code
     end
 

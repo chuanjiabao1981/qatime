cf511f711a5da9f02bf8ca12ffdc33664c71f69f
liyang610 <897430772@qq.com>
2016-08-16 10:01:12 +0800

    QT-280 修改老师/学生 添加资金记录

--- engines/payment/app/controllers/payment/users_controller.rb
+++ engines/payment/app/controllers/payment/users_controller.rb
@@ -3,6 +3,22 @@ require_dependency "payment/application_controller"
 module Payment
   class UsersController < ApplicationController
     def cash
+      if @current_resource.student?
+        @student = @current_resource
+        if params[:fee].nil?
+          @deposits = @current_resource.account.deposits.order(created_at: :desc).paginate(page: params[:page],:per_page => 10)
+        else
+          @consumption_records = @current_resource.account.consumption_records.order(created_at: :desc).paginate(page: params[:page],:per_page => 10)
+        end
+      elsif @current_resource.teacher?
+        @teacher = @current_resource
+        if params[:fee].nil?
+          @withdraws = @teacher.account.withdraws.order(created_at: :desc).paginate(page: params[:page],:per_page => 10)
+        else
+          @earning_records      = @teacher.account.earning_records.order(created_at: :desc).paginate(page: params[:page],:per_page => 10)
+        end
+      end
+
       @change_records = @current_resource.cash_account!.change_records.includes(billing: :target).paginate(page: params[:page])
     end
 

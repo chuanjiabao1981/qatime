e94072f7df9b854526a1549dcc82ab909b55c42a
xinshuaifeng <xinshuaifeng@126.com>
2016-08-18 04:08:30 +0800

    QT-292 资源下载分角色

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   layout "application"
   def download
+    render && return unless current_user
+    @softwares = Software.all
+    @softwares = @softwares.where(role: current_user.role) if current_user.student? || current_user.teacher?
   end
 end

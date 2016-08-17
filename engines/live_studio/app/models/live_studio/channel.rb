5f7aa334be2ece621b4eeeb318fa9a13e13faf7e
lukegit <wccxiaoan@gmail.com>
2016-08-17 10:01:17 +0800

    QT-224

--- engines/live_studio/app/models/live_studio/channel.rb
+++ engines/live_studio/app/models/live_studio/channel.rb
@@ -38,8 +38,8 @@ module LiveStudio
         push_streams.create(address: result['pushUrl'], protocol: 'rtmp')
         pull_streams.create(address: result['rtmpPullUrl'], protocol: 'rtmp')
       else
-        pull = 'rtmp://pa0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419?wsSecret=a3c84d0ecfdeb7434ffaa534607b9e8f&wsTime=1471330308'
-        push = 'rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419'
+        push = 'rtmp://pa0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419?wsSecret=a3c84d0ecfdeb7434ffaa534607b9e8f&wsTime=1471330308'
+        pull = 'rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419'
         push_streams.create(address: push, protocol: 'rtmp')
         pull_streams.create(address: pull, protocol: 'rtmp')
       end

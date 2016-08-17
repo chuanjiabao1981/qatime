7e5f65c4052a4ab7a466811a2fbc6e65b67c0155
lukegit <wccxiaoan@gmail.com>
2016-08-18 01:14:35 +0800

    QT-283 测试

---
+++ test/integration/payment/student_account_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+module Payment
+  class StudentAccountTest < ActionDispatch::IntegrationTest
+    def setup
+      @routes = Engine.routes
+      @headless = Headless.new
+      @headless.start
+      Capybara.current_driver = :selenium_chrome
+
+      @student = users(:student1)
+
+      new_log_in_as(@student)
+    end
+
+    def teardown
+      new_logout_as(@student)
+      Capybara.use_default_driver
+    end
+
+    test 'student account deposits test' do
+      visit payment.cash_user_path(@student)
+      click_on '充值记录'
+    end
+
+    test 'student account consumption_records test' do
+      visit payment.cash_user_path(@student)
+      click_on '消费记录'
+    end
+
+  end
+end

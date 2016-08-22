a19ce39cb967786b27f868e5d46c5f7b904696db
lukegit <wccxiaoan@gmail.com>
2016-08-22 17:49:52 +0800

    QT-291 邮箱找回密码测试

--- test/integration/password_test.rb
+++ test/integration/password_test.rb
@@ -11,13 +11,13 @@ class PasswordTest < ActionDispatch::IntegrationTest
     Capybara.use_default_driver
   end
 
-  test "unsigned password edit" do
+  test "login_mobile unsigned password edit" do
     user = users(:find_password_student1)
     visit root_path
     click_on "登录", match: :first
     click_on "找回密码", match: :first
 
-    fill_in :user_login_mobile, with: user.login_mobile
+    fill_in :user_login_account, with: user.login_mobile
     click_on "获取验证码", match: :first
     fill_in :user_captcha_confirmation, with: "1234"
 
@@ -25,6 +25,24 @@ class PasswordTest < ActionDispatch::IntegrationTest
     fill_in :user_password_confirmation, with: "pa123456"
 
     click_on "提交", match: :first
+    assert page.has_content?('更新成功'), '没有找回密码'
+  end
+
+  test "email unsigned password edit" do
+    user = users(:find_password_student2)
+    visit root_path
+    click_on "登录", match: :first
+    click_on "找回密码", match: :first
+
+    fill_in :user_login_account, with: user.email
+    click_on "获取验证码", match: :first
+    fill_in :user_captcha_confirmation, with: "1234"
+
+    fill_in :user_password, with: "pa123456"
+    fill_in :user_password_confirmation, with: "pa123456"
+
+    click_on "提交", match: :first
+    binding.pry
     assert page.has_content?('更新成功'), '没有找回密码'
   end
 end

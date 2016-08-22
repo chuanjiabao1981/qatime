efecf2f3bed22d16da85a81705aa4e54e04988e2
liyang610 <897430772@qq.com>
2016-08-22 13:51:34 +0800

    QT-282 修改login_mobile验证

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -55,6 +55,8 @@ class StudentsController < ApplicationController
     update_params = params.require(:student).permit(:password, :login_mobile, :email, :parent_phone)
 
     @student.password_required! if update_params[:password]
+    @student.update_register_required! if update_params[:login_mobile]
+
     @student.update(update_params)
     @update_attr = update_params.keys.first
   end

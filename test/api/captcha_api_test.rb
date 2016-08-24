570743d7bb24cc75b33868bbeade0964590d7434
liyang610 <897430772@qq.com>
2016-08-24 13:52:21 +0800

    QT-303 修改测试

--- test/api/captcha_api_test.rb
+++ test/api/captcha_api_test.rb
@@ -1,18 +1,10 @@
 require 'test_helper'
 class Qatime::CaptchaAPITest < ActionDispatch::IntegrationTest
-  def setup
-    @teacher = users(:teacher1)
-    post '/api/v1/sessions', email: @teacher.email,
-                             password: 'password',
-                             client_type: 'pc'
-    @remember_token = JSON.parse(response.body)['data']['remember_token']
-  end
-
   def app
     Rails.application
   end
 
-  test "POST /api/v1/captcha returns teacher's info" do
+  test "POST /api/v1/captcha send captcha" do
     post "/api/v1/captcha", {send_to: "15934836972", key: :register_captcha}
 
     assert_response :success

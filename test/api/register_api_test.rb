c0e3a508c7fa7c1138f14c2b427c16c758dff81b
liyang610 <897430772@qq.com>
2016-08-24 14:47:49 +0800

    QT-303 修改测试

--- test/api/register_api_test.rb
+++ test/api/register_api_test.rb
@@ -4,37 +4,6 @@ class Qatime::RegisterAPITest < ActionDispatch::IntegrationTest
     Rails.application
   end
 
-  # test "POST /api/v1/user/register register teacher" do
-  #   teacher_register_code = register_codes(:teacher_register_code)
-
-  #   post "/api/v1/captcha", {send_to: "13892920101", key: :register_captcha}
-  #   post "/api/v1/user/register", {login_mobile: "13892920101", captcha_confirmation: "1234", password: "pa123456", password_confirmation: "pa123456", register_code_value: teacher_register_code.value, accept: "1", type: "Teacher", client_type: "app"}
-
-  #   assert_response :success
-  #   res = JSON.parse(response.body)
-
-  #   assert_equal 1, res['status']
-
-  #   teacher = ::Teacher.find_by(login_mobile: res['data']['user']['login_mobile'])
-  #   remember_token = res['data']['remember_token']
-
-  #   img_file = fixture_file_upload("#{Rails.root}/test/integration/avatar.jpg", 'image/jpeg')
-  #   put "/api/v1/teachers/#{teacher.id}/profile", {name: "test_name", avatar: img_file, gender: "male", category: "小学", subject: "数学", birthday: "1999-01-01", desc: "desc test", email: "123@456.com", email_confirmation: "123@456.com"}, 'Remember-Token' => remember_token
-
-  #   assert_response :success
-  #   res = JSON.parse(response.body)
-  #   assert_equal 1, res['status']
-  #   assert_equal 17, res['data'].size
-
-  #   teacher.reload
-  #   assert_equal teacher.name, res['data']['name']
-  #   assert_equal teacher.avatar_url, res['data']['avatar_url']
-  #   assert_equal teacher.category, res['data']['category']
-  #   assert_equal teacher.subject, res['data']['subject']
-  #   assert_equal "1999-01-01", res['data']['birthday']
-  #   assert_equal teacher.desc, res['data']['desc']
-  # end
-
   test "POST /api/v1/user/register register student" do
     student_register_code = register_codes(:student_register_code)
 
@@ -62,5 +31,7 @@ class Qatime::RegisterAPITest < ActionDispatch::IntegrationTest
     assert_equal student.grade, res['data']['grade']
     assert_equal "1999-01-01", res['data']['birthday']
     assert_equal student.desc, res['data']['desc']
+    assert_equal student.email, res['data']['email']
+    assert_equal '13892920104', student.parent_phone
   end
 end

b974ad807163f7c318599237c93b60cb4756287c
liyang610 <897430772@qq.com>
2016-08-30 12:46:40 +0800

    QT-241 订单接口

--- test/api/payment/order_api_test.rb
+++ test/api/payment/order_api_test.rb
@@ -16,4 +16,23 @@ class Qatime::OrderApiTest < ActionDispatch::IntegrationTest
     assert_equal 1, res['status']
   end
 
+  test 'get order list' do
+    student = users(:student_with_order2)
+    post '/api/v1/sessions', email: student.email,
+         password: 'password',
+         client_type: 'pc'
+    remember_token = JSON.parse(response.body)['data']['remember_token']
+
+    get "/api/v1/payment/orders", {}, {'Remember-Token' => remember_token}
+    assert_response :success
+    res = JSON.parse(response.body)
+    assert_equal 1, res['status']
+    assert_equal 3, res['data'].size
+
+    get "/api/v1/payment/orders", {cate: "unpaid"}, {'Remember-Token' => remember_token}
+    assert_response :success
+    res = JSON.parse(response.body)
+    assert_equal 1, res['status']
+    assert_equal "unpaid", res['data'][0]['status']
+  end
 end

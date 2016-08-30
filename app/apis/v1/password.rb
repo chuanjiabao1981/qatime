e3549f6e3a97eba8be8653ea1ba0a5898389cf49
liyang610 <897430772@qq.com>
2016-08-30 16:23:30 +0800

    QT-325 用户找回密码接口

---
+++ app/apis/v1/password.rb
@@ -0,0 +1,33 @@
+module V1
+  # 密码接口
+  class Password < Base
+    resource :user do
+      desc 'find password.'
+      params do
+        requires :login_account, type: String, desc: '登录账户'
+        requires :captcha_confirmation, type: String, desc: '手机验证码'
+        requires :password, type: String, desc: '新密码'
+        requires :password_confirmation, type: String, desc: '密码确认'
+      end
+      put "find_password" do
+        password_params = ActionController::Parameters.new(params).permit(:login_account, :password, :password_confirmation, :captcha_confirmation)
+
+        user = User.find_by_login_account(password_params[:login_account]).try(:password_required!)
+        if user
+          captcha_manager = UserService::CaptchaManager.new(password_params[:login_account])
+          user.captcha = captcha_manager.captcha_of(:get_password_back)
+          if user.update_with_captcha(password_params)
+            captcha_manager.expire_captch(:get_password_back)
+            present user, with: Entities::User
+          else
+            raise(ActiveRecord::RecordInvalid.new(user))
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
+
---
+++ test/api/password_api_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+class Qatime::PasswordAPITest < ActionDispatch::IntegrationTest
+  def app
+    Rails.application
+  end
+
+  test "PUT /api/v1/users/:id/password find password by email" do
+    user = users(:student1)
+    login_account = user.email
+
+    post "/api/v1/captcha", {send_to: login_account, key: :get_password_back}
+    put "/api/v1/user/find_password", {login_account: login_account, captcha_confirmation: "1234", password: "pa123456", password_confirmation: "pa123456"}, 'Remember-Token' => @remember_token
+
+    assert_response :success
+    res = JSON.parse(response.body)
+    assert_equal 1, res['status']
+    user.reload
+    assert_equal(true, user.authenticate('pa123456').present?, '找回密码错误')
+  end
+
+  test "PUT /api/v1/users/user/find_password find password by login_mobile" do
+    user = users(:student1)
+    login_account = user.login_mobile
+
+    post "/api/v1/captcha", {send_to: login_account, key: :get_password_back}
+
+    put "/api/v1/user/find_password", {login_account: user.login_mobile,
+        captcha_confirmation: "1234", password: "pa1234567", password_confirmation: "pa1234567"}, 'Remember-Token' => @remember_token
+
+    assert_response :success
+    res = JSON.parse(response.body)
+    assert_equal 1, res['status']
+    user.reload
+    assert_equal(true, user.authenticate('pa1234567').present?, '找回密码错误')
+  end
+end

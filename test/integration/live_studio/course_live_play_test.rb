37fafc160bf5c06293a0103b5803b2b7124d9236
liyang610 <897430772@qq.com>
2016-08-31 14:16:37 +0800

    QT-319 辅导班文案修改

--- test/integration/live_studio/course_live_play_test.rb
+++ test/integration/live_studio/course_live_play_test.rb
@@ -42,14 +42,14 @@ module LiveStudio
       assert page.has_content?(@course.name), '辅导班名称显示不正确'
 
       assert page.has_content?('直播详情'), '直播详情按钮不存在'
-      assert find(:css, '#live_detail_area').has_content?('基本信息'), '基本信息按钮不存在'
-      assert find(:css, '#live_detail_area').has_content?('老师详情'), '老师详情按钮不存在'
+      assert find(:css, '#live_detail_area').has_content?('辅导概况'), '辅导概况按钮不存在'
+      assert find(:css, '#live_detail_area').has_content?('教师资料'), '教师资料按钮不存在'
       assert find(:css, '#live_detail_area').has_content?('课程列表'), '课程列表按钮不存在'
 
-      assert find(:css, '#detail_base_area').has_content?(@course.name), '基本信息显示不正确'
+      assert find(:css, '#detail_base_area').has_content?(@course.name), '辅导概况显示不正确'
 
       find(:css, '#detail_teacher_btn').click
-      assert find(:css, '#detail_teacher_area').has_content?(@course.teacher.name), '教师详情显示不正确'
+      assert find(:css, '#detail_teacher_area').has_content?(@course.teacher.name), '教师资料显示不正确'
 
       find(:css, '#detail_course_btn').click
       assert find(:css, '#detail_course_area').has_content?(@course.lessons.first.name), '课程列表显示不正确'

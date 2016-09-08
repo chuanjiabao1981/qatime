e883c9fe89a084cd6b39dec67c525ef77a4aca70
liyang610 <897430772@qq.com>
2016-09-08 15:35:22 +0800

    QT-245 修改页面

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -22,8 +22,6 @@ class NotificationsController < ApplicationController
       begin
         # 可以重定向到customized_course_action_record_path 然后再次重定向，不过这样重定向次数太多
 
-        if current_resource.type == "LiveStudio::CourseActionNotification"
-        end
         redirect_to send "#{@notification.notificationable.actionable.model_name.singular_route_key}_path",@notification.notificationable.actionable
       rescue
         redirect_to user_home_path

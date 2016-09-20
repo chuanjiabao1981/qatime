1ad51b84407b801eed2ad5f86eb148c8f5e59752
xinshuaifeng <xinshuaifeng@126.com>
2016-09-20 11:11:28 +0800

    QT-332 修改文件名

--- engines/payment/db/migrate/20160920021929_add_income_and_expenditure_to_cash_accounts.rb
+++ engines/payment/db/migrate/20160920021929_add_income_and_expenditure_to_cash_accounts.rb
@@ -1,4 +1,4 @@
-class AddIncomeAndExpenditureToCashAccount < ActiveRecord::Migration
+class AddIncomeAndExpenditureToCashAccounts < ActiveRecord::Migration
   def change
     add_column :payment_cash_accounts, :total_income, :decimal, precision: 8, scale: 2, default: 0
     add_column :payment_cash_accounts, :total_expenditure, :decimal, precision: 8, scale: 2, default: 0

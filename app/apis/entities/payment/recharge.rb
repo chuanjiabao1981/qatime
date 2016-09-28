e97c67615c066fbb279d28d42078c5dc28d1bdbe
xinshuaifeng <xinshuaifeng@126.com>
2016-09-28 16:55:01 +0800

    QT-332 try errors

--- app/apis/entities/payment/recharge.rb
+++ app/apis/entities/payment/recharge.rb
@@ -12,13 +12,13 @@ module Entities
       expose :updated_at
       expose :pay_at
       expose :prepay_id do |recharge|
-        recharge.remote_order.try(&:prepay_id)
+        recharge.remote_order.try(:prepay_id)
       end
       expose :nonce_str do |recharge|
-        recharge.remote_order.try(&:nonce_str)
+        recharge.remote_order.try(:nonce_str)
       end
       expose :app_pay_params do |recharge|
-        recharge.remote_order.try(&:app_pay_params)
+        recharge.remote_order.try(:app_pay_params)
       end
     end
   end

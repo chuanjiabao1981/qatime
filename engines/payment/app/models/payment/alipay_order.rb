c350e592f6c9667c84e69f826b906584e26ce515
xinshuaifeng <xinshuaifeng@126.com>
2016-09-28 18:41:27 +0800

    QT-332 支付宝移动支付参数

--- engines/payment/app/models/payment/alipay_order.rb
+++ engines/payment/app/models/payment/alipay_order.rb
@@ -15,6 +15,15 @@ module Payment
       notify_params[:trade_status] == "TRADE_FINISHED" ? pay! : fail!
     end
 
+    def app_pay_params
+      Alipay::Mobile::Service.mobile_securitypay_pay_string(
+        out_trade_no: order_no,
+        notify_url: order.notify_url,
+        subject: order.subject,
+        total_fee: amount
+      )
+    end
+
     private
 
     after_create :remote_sync

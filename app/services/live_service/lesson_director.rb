e20f24a795cb26aae6953c443f3a4d596eca396c
xinshuaifeng <xinshuaifeng@126.com>
2016-10-11 14:25:36 +0800

    QT-358 计时

--- app/services/live_service/lesson_director.rb
+++ app/services/live_service/lesson_director.rb
@@ -35,7 +35,7 @@ module LiveService
       @lesson.teacher_id = @course.teacher_id
       @lesson.live_count = @course.buy_tickets_count # 听课人数
       @lesson.live_end_at ||= Time.now
-      @lesson.real_time = @lesson.live_sessions.map(&:to_i).sum # 实际直播时间单位分钟
+      @lesson.real_time = @lesson.live_sessions.sum(:duration) # 实际直播时间单位分钟
       # 更新辅导课程完成数量
       @course.completed_lesson_count += 1
       @course.save!

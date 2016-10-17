aa097beffc483f4b12c70ad8c2fbf7eea293cff1
xinshuaifeng <xinshuaifeng@126.com>
2016-10-17 15:41:32 +0800

    QT-359 代码格式化

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,15 +1,13 @@
-
 class Video < ActiveRecord::Base
   require 'carrierwave/orm/activerecord'
-  belongs_to :videoable,polymorphic: true
+  belongs_to :videoable, polymorphic: true
   belongs_to :author, class_name: "User"
   include VideoConvert
 
   validates_presence_of :author
 
-  def self.update_videoable_info(videoable_item,videoable_type)
-    Video.where("token='#{videoable_item.token}'")
-        .update_all({videoable_id: videoable_item.id,videoable_type: videoable_type})
+  def self.update_videoable_info(videoable_item, videoable_type)
+    Video.where("token='#{videoable_item.token}'").update_all(videoable_id: videoable_item.id, videoable_type: videoable_type)
   end
 
   has_many :video_quoters
@@ -17,13 +15,11 @@ class Video < ActiveRecord::Base
 
   # 从网络获取视频时长
   def sync_duration!
-    result = %x(ffprobe -i #{name_url} -show_entries format=duration -v quiet -of csv="p=0")
+    result = `ffprobe -i #{name_url} -show_entries format=duration -v quiet -of csv="p=0"`
     self.duration = result.to_i
     save!
   end
-
 end
-
 
 class VideoChangedWhenConvertingException < Exception
 end

744d5fab4e818e8ef371e5a18e8ca8a49911829e
lukegit <wccxiaoan@gmail.com>
2016-10-31 15:47:21 +0800

    QT-377 测试

---
+++ test/api/public_info_api_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+class Qatime::PublicInfoAPITest < ActionDispatch::IntegrationTest
+
+  test "GET /api/v1/teachers/:id/profile" do
+    teacher = users(:teacher_for_taste_course)
+    get "/api/v1/teachers/#{teacher.id}/profile"
+    assert_response :success
+    res = JSON.parse(response.body)
+    assert_equal 1, res['status']
+    assert_equal 6, res['data'].count
+  end
+end

7c638e85ab25086ac035c1003e4e56dae845d33c
lukegit <wccxiaoan@gmail.com>
2016-11-01 18:35:57 +0800

    QT-381 推荐管理测试

--- test/integration/recommend/admin_manage_test.rb
+++ test/integration/recommend/admin_manage_test.rb
@@ -18,7 +18,7 @@ module Recommend
       Capybara.use_default_driver
     end
 
-    test 'admin manage positions ' do
+    test 'admin manage positions' do
       click_on '推荐管理'
       click_link 'new'
       fill_in :position_name, with: 'new_test'
@@ -26,6 +26,7 @@ module Recommend
       select '教师推荐', from: :position_klass_name
       click_on '新增推荐位'
       assert page.has_content?('Position was successfully created.')
+      click_on '推荐管理'
       accept_prompt(with: "Are you sure?") do
         click_link '启用', match: :first
       end
@@ -34,6 +35,18 @@ module Recommend
         click_link '删除', match: :first
       end
       assert page.has_content?('Position was successfully destroyed.')
+    end
+
+    test 'admin manage items' do
+      position = recommend_positions(:index_teacher_recommend)
+      visit recommend.admin_position_path(position)
+      click_link 'new'
+      fill_in :teacher_item_title, with: 'test'
+      fill_in :teacher_item_index, with: '1'
+      select 'teacher_one', from: :teacher_item_target_id
+      select '最热', from: :teacher_item_reason
+      click_on '新增教师推荐'
+      assert page.has_content?('Item was successfully created.')
     end
   end
 end

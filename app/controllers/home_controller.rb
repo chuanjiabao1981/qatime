ba7be2f0e6957484a5050a818b669ecfa18366cb
lukegit <wccxiaoan@gmail.com>
2016-11-02 16:38:45 +0800

    QT-400 首页 topbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   layout 'application_home'
   def index
     redirect_to user_home_path if signed_in?
+    @recommend_courses = Recommend::Position.all
+  end
+
+  private
+  def set_user
+    @user = current_user
   end
 end

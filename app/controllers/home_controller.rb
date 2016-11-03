e048922ab64e41fc6bce301f5528fd5813920810
lukegit <wccxiaoan@gmail.com>
2016-11-03 17:26:16 +0800

    QT-400 首页修改

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,19 @@
 class HomeController < ApplicationController
-  layout 'application_front'
   before_action :set_user
   def index
-    redirect_to action: :new_index if signed_in?
+    # if signed_in?
+    redirect_to action: :new_index
+    # else
+    #   render layout: 'application_home'
+    # end
   end
 
   def new_index
-    @recommend_courses = Recommend::Item.where(type: 'Recommend::LiveStudioCourseItem')
-    @recommend_teachers = Recommend::Item.where(type: 'Recommend::TeacherItem')
+    @recommend_courses = Recommend::Item.where(type: 'Recommend::LiveStudioCourseItem').limit(6)
+    @recommend_teachers = Recommend::Item.where(type: 'Recommend::TeacherItem').limit(5)
+    @user_path = @user.blank? ? signin_path : (!@user.student? && !@user.teacher? && 'javascript:void(0);')
+    @questions = Question.all.includes({learning_plan: :teachers},:vip_class,:student).order("created_at desc").limit(4)
+    render layout: 'application_front'
   end
 
   private

f5bc50d84a47d2a0dfb74a290955679e3884458f
lukegit <wccxiaoan@gmail.com>
2016-11-03 18:28:22 +0800

    QT-400 resolve review

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,8 +9,8 @@ class HomeController < ApplicationController
   end
 
   def new_index
-    @recommend_courses = Recommend::Item.where(type: 'Recommend::LiveStudioCourseItem').limit(6)
-    @recommend_teachers = Recommend::Item.where(type: 'Recommend::TeacherItem').limit(5)
+    @recommend_courses = Recommend::LiveStudioCourseItem.limit(6)
+    @recommend_teachers = Recommend::TeacherItem.limit(5)
     @user_path = @user.blank? ? signin_path : (!@user.student? && !@user.teacher? && 'javascript:void(0);')
     @questions = Question.all.includes({learning_plan: :teachers},:vip_class,:student).order("created_at desc").limit(4)
     render layout: 'application_front'

d973701e7ce795aa8446ecfca17ad823ddf94acf
xinshuaifeng <xinshuaifeng@126.com>
2016-11-03 20:21:25 +0800

    QT-377 教师公开页接口增加字段

--- app/apis/entities/teacher_profile.rb
+++ app/apis/entities/teacher_profile.rb
@@ -5,6 +5,8 @@ module Entities
     expose :desc
     expose :teaching_years
     expose :gender
+    expose :grade
+    expose :subject
     expose :avatar_url do |user,options|
       options[:size] == :info ? user.avatar_url(:ex_big) : user.avatar_url
     end

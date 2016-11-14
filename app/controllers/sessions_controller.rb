57f2f9135e8986b0b684bc8104fc6cb5873006da
lukegit <wccxiaoan@gmail.com>
2016-11-14 19:18:52 +0800

    QT-498 教师公开页

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:user][:password])
       sign_in(@user)
       flash[:info] = "欢迎登录!"
-      redirect_to user_home_path
+      redirect_to params[:redirect_url].blank? ? user_home_path : params[:redirect_url]
     else
       # @user = User.new(email: @user.try(:email))
        @user = User.new(login_mobile: @user.try(:login_account))

50cb4ae4f6ef6e91891555418de70c8e80c0e532
xinshuaifeng <xinshuaifeng@126.com>
2016-11-15 15:09:33 +0800

    创建多种协议流地址

--- engines/live_studio/app/models/live_studio/channel.rb
+++ engines/live_studio/app/models/live_studio/channel.rb
@@ -42,16 +42,26 @@ module LiveStudio
       if Rails.env.production? || Rails.env.test?
         push_streams.create(address: result['pushUrl'], protocol: 'rtmp')
         pull_streams.create(address: result['rtmpPullUrl'], protocol: 'rtmp')
+        pull_streams.create(address: result['hlsPullUrl'], protocol: 'hls')
+        pull_streams.create(address: result['httpPullUrl'], protocol: 'http')
       elsif use_for == 'board'
         push = 'rtmp://pa0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419?wsSecret=a3c84d0ecfdeb7434ffaa534607b9e8f&wsTime=1471330308'
         pull = 'rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419'
+        hls_pull = ''
+        http_pull = ''
         push_streams.create(address: push, protocol: 'rtmp')
         pull_streams.create(address: pull, protocol: 'rtmp')
+        pull_streams.create(address: hls_pull, protocol: 'hls')
+        pull_streams.create(address: http_pull, protocol: 'http')
       else
         push = 'rtmp://pa0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97?wsSecret=74c57894244754864cd0f07cc25ba4be&wsTime=1477448251'
         pull = 'rtmp://va0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97'
+        hls_pull = 'http://pullhlsa0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97/playlist.m3u8'
+        http_pull = 'http://va0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97.flv?netease=va0a19f55.live.126.net'
         push_streams.create(address: push, protocol: 'rtmp')
         pull_streams.create(address: pull, protocol: 'rtmp')
+        pull_streams.create(address: hls_pull, protocol: 'hls')
+        pull_streams.create(address: http_pull, protocol: 'http')
       end
     end
 
--- engines/live_studio/app/models/live_studio/course.rb
+++ engines/live_studio/app/models/live_studio/course.rb
@@ -82,8 +82,8 @@ module LiveStudio
     end
 
     # 白板拉流地址
-    def board_pull_stream
-      pull_streams.find {|stream| stream.use_for == 'board' }.try(:address)
+    def board_pull_stream(protocol = 'rtmp')
+      pull_streams.find {|stream| stream.use_for == 'board' && stream.protocol == protocol }.try(:address)
     end
 
     # 摄像头推流地址
@@ -92,8 +92,8 @@ module LiveStudio
     end
 
     # 摄像头拉流地址
-    def camera_pull_stream
-      pull_streams.find {|stream| stream.use_for == 'camera' }.try(:address)
+    def camera_pull_stream(protocol = 'rtmp')
+      pull_streams.find {|stream| stream.use_for == 'camera' && stream.protocol == protocol }.try(:address)
     end
 
     scope :for_sell, -> { where(status: [Course.statuses[:preview], Course.statuses[:teaching]]) }

ee5a2d7e1c8ce19976f11b69e25bf44b9a318085
lukegit <wccxiaoan@gmail.com>
2016-11-16 20:24:10 +0800

    QT-423 城市选择

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,17 +11,28 @@ class HomeController < ApplicationController
   end
 
   def new_index
+    set_city
     @recommend_courses = Recommend::LiveStudioCourseItem.limit(6)
     @recommend_teachers = Recommend::TeacherItem.limit(5)
     @user_path = @user.blank? ? signin_path : (!@user.student? && !@user.teacher? && 'javascript:void(0);')
   end
 
   def switch_city
-    @all_cities = City.all
+    @hash_cities = ::Util.group_cities
+    @selected_cities = cookies[:selected_cities].try(:split, '-').try(:sort){ |x,y| y <=> x }
   end
 
   private
   def set_user
     @user = current_user
+  end
+
+  def set_city
+    @city = City.find_by(id: params[:city_id]) || City.find_by(name: params[:city_name])
+    return if @city.blank?
+    selected_cities = cookies[:selected_cities].try(:split, '-').try(:sort){ |x,y| y <=> x } || []
+    selected_cities = selected_cities.insert(0, @city.name).sort
+    cookies[:selected_cities] = selected_cities.uniq.try(:join, '-')
+    @city
   end
 end

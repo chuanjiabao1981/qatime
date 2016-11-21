44ed31af36a2ee89ecaaa0d8e01f650c3ffb7d7d
xinshuaifeng <xinshuaifeng@126.com>
2016-11-21 10:15:21 +0800

    flash 检测

--- engines/live_studio/app/assets/javascripts/live_studio/chat.js
+++ engines/live_studio/app/assets/javascripts/live_studio/chat.js
@@ -290,7 +290,33 @@ window.currentTeam = {};
   }
 })(this);
 
+//判断flash是否安装方法
+function flashChecker(){
+  var hasFlash=0;　　　　//是否安装了flash
+  var flashVersion=0;　　//flash版本
 
+  if(document.all){
+    var swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+    if(swf) {
+      hasFlash=1;
+      VSwf=swf.GetVariable("$version");
+      flashVersion=parseInt(VSwf.split(" ")[1].split(",")[0]);
+    }
+  } else {
+    if(navigator.plugins && navigator.plugins.length > 0) {
+      var swf=navigator.plugins["Shockwave Flash"];
+      if(swf) {
+        hasFlash=1;
+        var words = swf.description.split(" ");
+        for(var i = 0; i < words.length; ++i) {
+          if (isNaN(parseInt(words[i]))) continue;
+          flashVersion = parseInt(words[i]);
+        }
+      }
+    }
+  }
+  return {f:hasFlash,v:flashVersion};
+}
 
 function refreshTeamMembersUI(teamId, fn) {
   if(teamId != currentTeam.id) return;

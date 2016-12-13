e5694766669c04dba28247529fadad97790e0b8e
xinshuaifeng <xinshuaifeng@126.com>
2016-12-13 11:21:47 +0800

    QT-666 弹幕报错

--- engines/live_studio/app/assets/javascripts/live_studio/chat.js
+++ engines/live_studio/app/assets/javascripts/live_studio/chat.js
@@ -402,7 +402,7 @@ function appendMsg(msg, messageClass) {
 
   // 显示弹幕
   var now = new Date().getTime();
-  if(currentTeam.barrage.active && now - msg.time < 5000) {
+  if(currentTeam.barrage && currentTeam.barrage.active && now - msg.time < 5000) {
     currentTeam.barrage.show($.replaceChatMsg(msg.text));
   }
 

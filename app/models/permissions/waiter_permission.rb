584cdf24d7727aedf9b5690c8ce95e707c25add5
lukegit <wccxiaoan@gmail.com>
2016-12-30 17:25:35 +0800

    QT-716 waiter & seller

--- app/models/permissions/waiter_permission.rb
+++ app/models/permissions/waiter_permission.rb
@@ -8,6 +8,14 @@ module Permissions
       allow "waiters/home", [:main]
 
       allow :waiters, [:customized_courses]
+      allow :teachers,[:index,:show,:search,:pass,:unpass,
+                       :students,:curriculums,:info,:questions,:topics,:lessons_state,:homeworks,
+                       :exercises,:solutions,:customized_tutorial_topics,:notifications,
+                       :customized_courses,:profile]
+      allow :students,[:index,:search,:show,
+                       :info,:teachers,:customized_courses,:homeworks,
+                       :solutions,:account,:customized_tutorial_topics,:questions,:notifications]
+
       allow :customized_courses, [:show,:edit,:update,:teachers,:topics,:homeworks,:solutions, :get_sale_price] do |customized_course|
         user && customized_course && user.customized_courses.include?(customized_course)
       end

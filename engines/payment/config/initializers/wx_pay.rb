d3454a6240ddebe9d6285fca68f04d81cf704830
lukegit <wccxiaoan@gmail.com>
2017-01-04 18:42:40 +0800

    QT-310 证书

--- engines/payment/config/initializers/wx_pay.rb
+++ engines/payment/config/initializers/wx_pay.rb
@@ -1,11 +1,13 @@
 # required
+require 'openssl'
 WxPay.appid = WECHAT_CONFIG['pay_appid']
 WxPay.key = WECHAT_CONFIG['key']
 WxPay.mch_id = WECHAT_CONFIG['mch_id']
 
 # cert, see https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=4_3
 # using PCKS12 for rollback
-# WxPay.set_apiclient_by_pkcs12(File.read(pkcs12_filepath), pass)
+WxPay.set_apiclient_by_pkcs12(File.read("#{Rails.root}/config/apiclient_cert.p12"), WECHAT_CONFIG['mch_id'])
 
+WxPay.appsecret = 'YOUR_SECRET'
 # optional - configurations for RestClient timeout, etc.
 WxPay.extra_rest_client_options = {timeout: 20, open_timeout: 5}

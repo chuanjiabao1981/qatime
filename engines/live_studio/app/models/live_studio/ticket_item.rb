1f12715d71f960dbcf7f53ee86119c111a8e0a37
xinshuaifeng <xinshuaifeng@126.com>
2017-01-12 18:02:52 +0800

    QT-784 退款状态

--- engines/live_studio/app/models/live_studio/ticket_item.rb
+++ engines/live_studio/app/models/live_studio/ticket_item.rb
@@ -1,5 +1,12 @@
 module LiveStudio
   class TicketItem < ActiveRecord::Base
+    enum status: {
+      unused: 0, # 未使用
+      used: 1, # 已使用
+      refunding: 98, # 退款中
+      refunded: 99 # 已退款
+    }
+
     belongs_to :lesson
   end
 end

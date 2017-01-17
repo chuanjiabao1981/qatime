42a59e748d3948ff7feaad6eadfea7d86d73e41e
xinshuaifeng <xinshuaifeng@126.com>
2017-01-17 20:14:02 +0800

    QT-776 课程排序

--- engines/live_studio/app/controllers/live_studio/lessons_controller.rb
+++ engines/live_studio/app/controllers/live_studio/lessons_controller.rb
@@ -48,6 +48,7 @@ module LiveStudio
       @course = @lesson.course
       @video = @lesson.channel_videos.where(video_for: 0).first
       @lessons = Lesson.where(course_id: @lesson.course_id, replay_status: LiveStudio::Lesson.replay_statuses[:synced])
+      @lessons = @lessons.order(:class_date, :live_start_at, :live_end_at)
       LiveStudio::PlayRecord.init_play(current_user, @lesson.course, @lesson)
       # 临时解决方案
       @paly_records = LiveStudio::PlayRecord.where(lesson_id: @lessons.map(&:id),

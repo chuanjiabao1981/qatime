a2492084e785287f996cf670849017b54c2bc2e2
xinshuaifeng <xinshuaifeng@126.com>
2017-01-17 13:22:13 +0800

    QT-776 任务执行错误

--- app/workers/channel_worker.rb
+++ app/workers/channel_worker.rb
@@ -3,7 +3,7 @@ class ChannelWorkder
 
   # 更新辅导班录制视频列表
   def perform(course_id)
-    LiveStudio::Course.find(course_id)
+    course = LiveStudio::Course.find(course_id)
     course.channels.each do |channel|
       channel.set_always_recorded && channel.update_video_list
     end

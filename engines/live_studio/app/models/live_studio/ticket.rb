f171c95485b4be4d6f8157e1aeb339d6afa83fa9
xinshuaifeng <xinshuaifeng@126.com>
2017-01-18 16:25:29 +0800

    QT-776 辅导班结束了不过期ticket

--- engines/live_studio/app/models/live_studio/ticket.rb
+++ engines/live_studio/app/models/live_studio/ticket.rb
@@ -47,6 +47,7 @@ module LiveStudio
 
     # 增加使用次数
     def inc_used_count!(_urgent = false)
+      return if course.finished_lessons_count >= course.lessons_count
       lock!
       self.used_count += 1
       used! if used_count >= buy_count

c462b2574f1c672dbf497e354826a0aaeb6b3057
xinshuaifeng <xinshuaifeng@126.com>
2017-01-18 19:42:22 +0800

    QT-776 试听不可以观看回放

--- engines/live_studio/app/models/live_studio/lesson.rb
+++ engines/live_studio/app/models/live_studio/lesson.rb
@@ -276,6 +276,7 @@ module LiveStudio
 
     # 是否可观看回放
     def replayable_for?(user)
+      return false unless course.buy_tickets.where(student_id: user.id).available.exists?
       return false unless course.play_authorize(user, nil)
       play_records.where(play_type: LiveStudio::PlayRecord.play_types[:replay],
                          user_id: user.id).where('created_at < ?', Date.today).count < LiveStudio::ChannelVideo::TOTAL_REPLAY

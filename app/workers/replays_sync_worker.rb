1e2d44130ff3ccf0eeba66e18550eaf351728c7a
xinshuaifeng <xinshuaifeng@126.com>
2017-01-18 11:10:53 +0800

    QT-776 回放同步失败重试

--- app/workers/replays_sync_worker.rb
+++ app/workers/replays_sync_worker.rb
@@ -3,8 +3,10 @@ class ReplaysSyncWorker
   include Sidekiq::Worker
 
   # 更新辅导班录制视频列表
-  def perform(lesson_id)
+  def perform(lesson_id, retry_times = 0)
     lesson = LiveStudio::Lesson.find(lesson_id)
     lesson.sync_replays
+    return unless lesson.channel_videos.count > 0
+    ReplaysSyncWorker.perform_async(lesson_id, retry_times + 1) if retry_times < 5
   end
 end

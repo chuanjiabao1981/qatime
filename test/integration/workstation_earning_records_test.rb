de1d7ba8af668226f337620062f49b170ff84e7d
xinshuaifeng <xinshuaifeng@126.com>
2017-02-08 20:02:59 +0800

    QT-712 收入详情集成测试

--- test/integration/workstation_earning_records_test.rb
+++ test/integration/workstation_earning_records_test.rb
@@ -25,4 +25,22 @@ class WorkstationEarningRecordsTest < ActionDispatch::IntegrationTest
     click_on "查询"
     assert page.has_content?("共计 ￥ 55.0"), "最近一周过滤后总收入显示不正确"
   end
+
+  test "manager view earning_records detail" do
+    @login_user = users(:manager_zhuji)
+    log_in_as(@login_user)
+    click_on '收入记录'
+    record1 = payment_change_records(:workstation_earning1)
+    record2 = payment_change_records(:workstation_earning5)
+
+    find("#item-#{record1.id}").click_link('查看明细')
+    assert page.has_content?("课程单价： ￥ 50.0"), "辅导班收入明细显示不正确"
+    assert page.has_content?("服务费标准： ￥ 0.1/分钟"), "辅导班收入明细显示不正确"
+    assert page.has_content?("教师分成： 80%"), "辅导班收入明细显示不正确"
+    assert page.has_content?("教师收入： ￥ 150.4"), "辅导班收入明细显示不正确"
+    find("#item-#{record2.id}").click_link('查看明细')
+    assert page.has_content?("课程收费标准： ￥ 54.0/小时"), "专属课程收入明细显示不正确"
+    assert page.has_content?("教师收入： ￥ 1.5"), "专属课程收入明细显示不正确"
+    assert page.has_content?("工作站收入： ￥ 40.0"), "专属课程收入明细显示不正确"
+  end
 end

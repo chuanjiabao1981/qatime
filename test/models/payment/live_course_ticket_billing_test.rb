39dae494cb5c015265daba5592026df2b35cff8b
xinshuaifeng <xinshuaifeng@126.com>
2017-02-21 15:33:03 +0800

    QT-846 辅导班分成百分比不正确结账测试

--- test/models/payment/live_course_ticket_billing_test.rb
+++ test/models/payment/live_course_ticket_billing_test.rb
@@ -24,7 +24,7 @@ module Payment
       assert_equal lesson.course.teacher.id, b.from_user.id, "账单参与者记录错误"
       assert_difference "Payment::LiveCourseTicketBilling.last.billing_items.count", 6, '账单条目数量不正确' do
         assert_difference "@system_account.reload.balance.to_f", -42.3, "系统收入不正确" do
-          assert_difference "@publish_account.reload.balance.to_f", 4.7, "发心上收入不正确" do
+          assert_difference "@publish_account.reload.balance.to_f", 4.7, "发行商收入不正确" do
             assert_difference "@sell_account.reload.balance.to_f", 14.1, "经销商收入不正确" do
               assert_difference "@teacher_account.reload.balance.to_f", 23.5, "教师收入不正确" do
                 b.billing
@@ -52,7 +52,7 @@ module Payment
       assert_equal lesson.course.teacher.id, b.from_user.id, "账单参与者记录错误"
       assert_difference "Payment::LiveCourseTicketBilling.last.billing_items.count", 6, '账单条目数量不正确' do
         assert_difference "@system_account.reload.balance.to_f", -42.3, "系统收入不正确" do
-          assert_difference "@publish_account.reload.balance.to_f", 4.7, "发心上收入不正确" do
+          assert_difference "@publish_account.reload.balance.to_f", 4.7, "发行商收入不正确" do
             assert_difference "@sell_account.reload.balance.to_f", 14.1, "经销商收入不正确" do
               assert_difference "@teacher_account.reload.balance.to_f", 23.5, "教师收入不正确" do
                 b.billing
@@ -60,6 +60,21 @@ module Payment
             end
           end
         end
+      end
+    end
+
+    # 测试错误分成比例结账
+    test 'test billing a ' do
+      lesson = live_studio_lessons(:lesson_of_billing_fail_course_one)
+      ticket = live_studio_tickets(:ticket_of_billing_fail_course_one)
+      assert_difference "Payment::LiveCourseTicketBilling.count", 1, "账单创建失败" do
+        Payment::LiveCourseTicketBilling.create(target: lesson, from_user: lesson.teacher, ticket: ticket)
+      end
+      b = Payment::LiveCourseTicketBilling.last
+      assert_equal 50.0, b.total_money.to_f, "账单金额不正确"
+      assert_equal lesson.course.teacher.id, b.from_user.id, "账单参与者记录错误"
+      assert_raises(Payment::TotalPercentInvalid) do
+        b.billing
       end
     end
   end

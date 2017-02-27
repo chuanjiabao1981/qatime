f40e78b98e5b0aac376e0cfe2e7c093d766eee9a
xinshuaifeng <xinshuaifeng@126.com>
2017-02-27 15:58:44 +0800

    QT-855 使用微信授权

--- app/controllers/wap/application_controller.rb
+++ app/controllers/wap/application_controller.rb
@@ -1,3 +1,8 @@
 class Wap::ApplicationController < ApplicationController
   layout 'wap'
+
+  def authorize
+    redirect_to '/auth/wechat' unless current_user.present?
+    redirect_to '/auth/wechat' if current_user.wechat_users.blank?
+  end
 end

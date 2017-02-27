23c92161e2127a0ed854ee3d72639a7c686f7810
xinshuaifeng <xinshuaifeng@126.com>
2017-02-27 14:43:57 +0800

    不加载证书

--- engines/payment/config/initializers/wx_pay.rb
+++ engines/payment/config/initializers/wx_pay.rb
@@ -5,7 +5,7 @@ WxPay.mch_id = WECHAT_CONFIG['mch_id']
 
 # cert, see https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=4_3
 # using PCKS12 for rollback
-WxPay.set_apiclient_by_pkcs12(WX_CERT, WECHAT_CONFIG['mch_id'])
+# WxPay.set_apiclient_by_pkcs12(WX_CERT, WECHAT_CONFIG['mch_id'])
 
 # WxPay.appsecret = 'YOUR_SECRET'
 # optional - configurations for RestClient timeout, etc.

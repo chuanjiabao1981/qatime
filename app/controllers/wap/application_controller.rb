181028021ccc1363000a169953778daf55808ba7
xinshuaifeng <xinshuaifeng@126.com>
2017-02-28 09:43:47 +0800

    QT-855 微信授权

--- app/controllers/wap/application_controller.rb
+++ app/controllers/wap/application_controller.rb
@@ -7,14 +7,10 @@ class Wap::ApplicationController < ApplicationController
   end
 
   def openid_required!
-    redirect_to oauth_url(request.original_url) if cookies[:openid].blank?
+    if cookies[:openid].blank?
+      session[:return_to] = request.original_url
+      redirect_to '/auth/wechat'
+    end
     @openid = cookies[:openid]
-  end
-
-  private
-  helper_method :oauth_url
-  def oauth_url(redirect_uri = nil)
-    redirect_uri = redirect_uri.to_query('redirect_uri') if redirect_uri.present?
-    "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{WECHAT_CONFIG['appid']}&#{redirect_uri}&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
   end
 end

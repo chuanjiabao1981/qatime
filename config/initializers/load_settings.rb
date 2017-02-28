bd4441736a89dff8a3c2127e2bacc1ef0d7013b3
xinshuaifeng <xinshuaifeng@126.com>
2017-02-28 17:09:17 +0800

    QT-855 多个退款证书

--- config/initializers/load_settings.rb
+++ config/initializers/load_settings.rb
@@ -1,3 +1,6 @@
 Settings = Qatime::Application.config_for(:settings).deep_symbolize_keys
 WechatSetting = Qatime::Application.config_for(:wechat).deep_symbolize_keys
-p WechatSetting[:wap]
+
+WechatSetting[:wap_cert] = [File.read(File.expand_path("../../#{WechatSetting[:wap][:cert]}", __FILE__)), WechatSetting[:wap][:mch_id]]
+WechatSetting[:app_cert] = [File.read(File.expand_path("../../#{WechatSetting[:app][:cert]}", __FILE__)), WechatSetting[:app][:mch_id]]
+WechatSetting[:web_cert] = [File.read(File.expand_path("../../#{WechatSetting[:web][:cert]}", __FILE__)), WechatSetting[:web][:mch_id]]

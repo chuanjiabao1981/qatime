ccacf72b25167dfc47d474b0b30840ba06e3abd2
xinshuaifeng <xinshuaifeng@126.com>
2017-03-01 18:21:27 +0800

    QT-858 购买金额

--- app/controllers/wap/live_studio/orders_controller.rb
+++ app/controllers/wap/live_studio/orders_controller.rb
@@ -8,10 +8,15 @@ class Wap::LiveStudio::OrdersController < Wap::ApplicationController
   end
 
   def create
+    # 用户之前的未支付订单 更新为无效订单
+    waste_orders = Payment::Order.where(user: current_user, status: 0, product: @course)
+    waste_orders.update_all(status: 99) if waste_orders.present?
     @order = Payment::Order.new(order_params.merge(user: current_user,
                                                    remote_ip: request.remote_ip,
                                                    source: 'wap',
                                                    openid: @openid))
+    @coupon = Payment::Coupon.find_by(code: @order.coupon_code) if @order.coupon_code.present?
+    @order.amount = @course.coupon_price(@coupon)
     if @order.save
       redirect_to wap_payment_order_path(@order.transaction_no)
     else
--- engines/payment/app/models/payment/weixin_order.rb
+++ engines/payment/app/models/payment/weixin_order.rb
@@ -36,7 +36,6 @@ module Payment
 
     after_create :remote_sync
     def remote_sync
-      return if Rails.env.development?
       return if Rails.env.test? || order.created_at < 3.hours.ago
       r = WxPay::Service.invoke_unifiedorder(remote_params, weixin_options)
       remote_result(r)

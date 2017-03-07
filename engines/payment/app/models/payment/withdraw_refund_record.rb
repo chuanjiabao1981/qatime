b1013651919cf4fea0e5542d98fa310ddd5c88c5
xinshuaifeng <xinshuaifeng@126.com>
2017-03-07 20:58:36 +0800

    QT-846 充值提现操作测试

---
+++ engines/payment/app/models/payment/withdraw_refund_record.rb
@@ -0,0 +1,5 @@
+# 账户流水 - 提现
+module Payment
+  class WithdrawRefundRecord < ChangeRecord
+  end
+end
--- test/services/account_service/cash_test.rb
+++ test/services/account_service/cash_test.rb
@@ -5,19 +5,51 @@ module AccountServiceTest
     # 充值测试
     test 'recharge cash' do
       @account = payment_cash_accounts(:student_balance_account)
-      manager = AccountService::CashManager.new(account)
+      manager = AccountService::CashManager.new(@account)
       # 充值成功
       assert_difference "@account.reload.balance.to_f", 100, "充值失败" do
         assert_difference "Payment::RechargeRecord.count", 1, "充值失败" do
-          manager.increase('Payment::Recharge', 100, nil)
+          manager.increase('Payment::RechargeRecord', 100, nil)
         end
       end
 
       # 充值失败
       assert_no_difference "@account.reload.balance.to_f", "充值没有回退" do
         assert_no_difference "Payment::RechargeRecord.count", "充值没有回退" do
-          manager.increase('Payment::Recharge', 100, nil) do
-            raise StandarError, '出错了'
+          assert_raises(StandardError) do
+            manager.increase('Payment::RechargeRecord', 100, nil) do
+              raise StandardError, '出错了'
+            end
+          end
+        end
+      end
+    end
+
+    # 提现测试
+    test 'withdraw cash' do
+      @account = payment_cash_accounts(:teacher_account)
+      manager = AccountService::CashManager.new(@account)
+      # 提现扣款成功
+      assert_difference "@account.reload.balance.to_f", -100, "提现失败" do
+        assert_difference "Payment::WithdrawRecord.count", 1, "提现失败" do
+          manager.decrease('Payment::WithdrawRecord', 100, nil)
+        end
+      end
+
+      # 提现退款
+      assert_difference "@account.reload.balance.to_f", 100, "提现退款失败" do
+        assert_difference "Payment::WithdrawRefundRecord.count", 1, "提现退款失败" do
+          manager.increase('Payment::WithdrawRefundRecord', 100, nil)
+        end
+      end
+
+      # 提现扣款失败
+      assert_no_difference "@account.reload.balance.to_f", "提现没有回退" do
+        assert_no_difference "Payment::WithdrawRecord.count", "提现没有回退" do
+          assert_raises(StandardError) do
+            manager.increase('Payment::WithdrawRecord', 100, nil) do
+              raise StandardError, '出错了'
+            end
           end
         end
       end

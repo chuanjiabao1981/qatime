b757ec73b008dbb74f6e8d16cacd4c3a87193fe5
liugang <501225658@qq.com>
2017-03-12 11:48:54 +0800

    QT-883 销售统计 测试

--- test/integration/manager_workstations_test.rb
+++ test/integration/manager_workstations_test.rb
@@ -23,4 +23,25 @@ class ManagerWorkstationsTest < ActionDispatch::IntegrationTest
     assert page.has_content?('可提现金额')
     assert page.has_link?('申请提现')
   end
+
+  test "visit workstation statistics" do
+    workstation_one = workstations(:workstation_one)
+    visit statistics_station_workstation_path(workstation_one)
+    assert page.has_link?('销售统计')
+    assert page.has_link?('售出记录')
+    assert page.has_link?('退款记录')
+    assert page.has_content?('销售总额')
+    assert page.has_content?('预计销售总收入')
+    assert page.has_select?('statistics_days')
+
+    select('近2月', from: 'statistics_days')
+
+    click_on '退款记录'
+    assert page.has_content?('退款金额')
+    assert page.has_content?('销售收入减少')
+
+    click_on '售出记录'
+    assert page.has_content?('优惠价格')
+    assert page.has_content?('销售收入增加')
+  end
 end

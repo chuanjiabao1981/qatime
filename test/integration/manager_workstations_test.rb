7163af91fa62a201d996b5105531faaa0584d63b
liugang <501225658@qq.com>
2017-03-12 12:49:12 +0800

    QT-879 出入账测试

--- test/integration/manager_workstations_test.rb
+++ test/integration/manager_workstations_test.rb
@@ -23,4 +23,19 @@ class ManagerWorkstationsTest < ActionDispatch::IntegrationTest
     assert page.has_content?('可提现金额')
     assert page.has_link?('申请提现')
   end
+
+  test "visit workstation change_records" do
+    workstation_one = workstations(:workstation_one)
+    visit change_records_station_workstation_path(workstation_one)
+    assert page.has_link?('出入账记录')
+    assert page.has_link?('出账记录')
+    assert page.has_link?('入账记录')
+    click_on '出账记录'
+    assert page.has_content?('支出金额')
+    assert page.has_content?('支出类型')
+
+    click_on '入账记录'
+    assert page.has_content?('收入金额')
+    assert page.has_content?('收入类型')
+  end
 end

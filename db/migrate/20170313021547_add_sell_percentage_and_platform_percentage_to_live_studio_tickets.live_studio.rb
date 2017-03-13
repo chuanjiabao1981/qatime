173001061241a6b9d31a5546740d39af066885a7
xinshuaifeng <xinshuaifeng@126.com>
2017-03-13 10:25:58 +0800

    QT-886 购买辅导班记录分成

---
+++ db/migrate/20170313021547_add_sell_percentage_and_platform_percentage_to_live_studio_tickets.live_studio.rb
@@ -0,0 +1,11 @@
+# This migration comes from live_studio (originally 20170313021257)
+class AddSellPercentageAndPlatformPercentageToLiveStudioTickets < ActiveRecord::Migration
+  def change
+    remove_column :live_studio_tickets, :cross_region_percentage
+    remove_column :live_studio_tickets, :sell_channel_id
+    remove_reference :live_studio_tickets, :channel_owner
+
+    add_column :live_studio_tickets, :sell_percentage, :integer, default: 0
+    add_column :live_studio_tickets, :platform_percentage, :integer, default: 0
+  end
+end

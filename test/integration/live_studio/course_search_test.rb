6dfcb9aff34fb8587caeed27b14a6c5f77ab26c2
xinshuaifeng <xinshuaifeng@126.com>
2017-04-06 11:43:49 +0800

    QT-985 标签分类测试

--- test/integration/live_studio/course_search_test.rb
+++ test/integration/live_studio/course_search_test.rb
@@ -87,5 +87,29 @@ module LiveStudio
       click_on '最新'
       assert_equal '最新 ▼', page.find('.nav-news.active').text
     end
+
+    # 标签过滤测试
+    test 'filter tag by subject and grade' do
+      assert_equal 20, all(".aside-list-nav a").size, "没有显示所有标签"
+      find('.nav-grade').hover
+      click_on '高一'
+      assert page.has_no_content?("高考")
+      assert page.has_no_content?("高考志愿")
+      assert page.has_no_content?("奥数竞赛")
+      find('.nav-grade').hover
+      click_on '高三'
+      assert page.has_content?("高考")
+      assert page.has_content?("高考志愿")
+      assert page.has_no_content?("奥数竞赛")
+      find('.nav-subject').hover
+      click_on '英语'
+      assert page.has_content?("高考")
+      assert page.has_content?("高考志愿")
+      assert page.has_content?("外教")
+      click_on '数学'
+      assert page.has_content?("高考")
+      assert page.has_content?("高考志愿")
+      assert page.has_no_content?("外教")
+    end
   end
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -2,11 +2,9 @@ require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   test 'category of' do
-    tag = tags(:高考)
-    # assert_equal 19, Tag.category_of.count, "不过滤标签查询错误"
-    # assert_equal 15, Tag.category_of('初三').count, "标签查询数据错误"
-    # assert_equal 13, Tag.category_of('语文', '初三').count, "多个标签数据返回错误"
-    # assert_equal 19, Tag.category_of('').count, "空分类数据查询错误"
-
+    assert_equal 19, Tag.category_of.count, "不过滤标签查询错误"
+    assert_equal 14, Tag.category_of('初三').count, "标签查询数据错误"
+    assert_equal 11, Tag.category_of('语文', '初三').count, "多个标签数据返回错误"
+    assert_equal 19, Tag.category_of('').count, "空分类数据查询错误"
   end
 end

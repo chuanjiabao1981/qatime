fa6167238e539d25c6cba336cc431d61be8635a4
xinshuaifeng <xinshuaifeng@126.com>
2017-04-07 17:51:41 +0800

    QT-996 消费方式复制

--- engines/payment/app/models/payment/consumption_record.rb
+++ engines/payment/app/models/payment/consumption_record.rb
@@ -1,5 +1,12 @@
 module Payment
   class ConsumptionRecord < ChangeRecord
     enumerize :change_type, in: { account: 0, alipay: 1, weixin: 2 }, default: :account
+
+    before_validation :copy_change_type, on: :create
+    def copy_change_type
+      self.change_type = business.pay_type if business.is_a?(Payment::Order)
+    rescue StandardError => e
+      p e
+    end
   end
 end

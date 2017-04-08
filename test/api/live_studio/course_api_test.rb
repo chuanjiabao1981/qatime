ff09dec5c146f0d70d4d8608a6c0a1f0aa159172
liugang <501225658@qq.com>
2017-04-08 16:28:29 +0800

    修改 标签数量测试

--- test/api/live_studio/course_api_test.rb
+++ test/api/live_studio/course_api_test.rb
@@ -316,7 +316,7 @@ class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status'], "请求出错 #{res}"
-    assert_equal res['data'].count, 20, "标签获取失败"
+    assert_equal res['data'].count, 19, "标签获取失败"
   end
 
   # 新的搜索接口
@@ -325,6 +325,6 @@ class Qatime::CoursesAPITest < ActionDispatch::IntegrationTest
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status'], "请求出错 #{res}"
-    assert_equal res['data'].count, 2, "标签获取失败"
+    assert_equal res['data'].count, 3, "标签获取失败"
   end
 end

499a1bb91c98f51cded169a0c1755db077cef417
liugang <501225658@qq.com>
2017-04-14 13:41:36 +0800

    QT-1001 api视频课搜索 测试

--- app/apis/entities/live_studio/student_video_course.rb
+++ app/apis/entities/live_studio/student_video_course.rb
@@ -2,10 +2,6 @@ module Entities
   module LiveStudio
     class StudentVideoCourse < Entities::LiveStudio::VideoCourse
 
-      with_options(format_with: :local_timestamp) do
-        expose :start_at, as: :preview_time
-      end
-
       expose :is_bought do |course, options|
         course.bought_by?(options[:current_user])
       end

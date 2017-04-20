e9b34ad8db4910289c28a65716eb6e28a7135ee0
xinshuaifeng <xinshuaifeng@126.com>
2017-04-20 14:23:54 +0800

    QT-1048 视频课通知集成测试

--- test/integration/live_studio/video_course_manage_test.rb
+++ test/integration/live_studio/video_course_manage_test.rb
@@ -35,10 +35,7 @@ module LiveStudio
       click_on '保存'
       assert page.has_content?('必须小于或等于 87'), "分成比例验证失效"
       fill_in 'video_course_teacher_percentage', with: 60
-
-      assert_difference "@video_course.teacher.reload.notifications.count", 1, "老师没有收到发布通知" do
-        click_on '保存并发布'
-      end
+      click_on '保存并发布'
       assert @video_course.reload.published?, "保存发布失败"
     end
 
@@ -52,14 +49,8 @@ module LiveStudio
       click_on '视频课'
       click_on '课程管理'
       click_on '已创建'
-      assert_difference "@video_course.teacher.reload.notifications.count", 1, "老师没有收到发布通知" do
-        find_link("发布", href: live_studio.publish_station_workstation_video_course_path(@workstation, @video_course)).click
-      end
+      find_link("发布", href: live_studio.publish_station_workstation_video_course_path(@workstation, @video_course)).click
       assert @video_course.reload.published?, "发布失败"
-
-      visit user_notifications_path(@video_course.teacher)
-      binding.pry
-      assert page.has_content?(""), "通知显示错误"
     end
   end
 end

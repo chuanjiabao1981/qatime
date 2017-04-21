f37fb41983be302697d3addd7e95f1d45a5d69f5
xinshuaifeng <xinshuaifeng@126.com>
2017-04-21 14:30:35 +0800

    QT-870 手机页面购买

--- app/assets/javascripts/wap.js
+++ app/assets/javascripts/wap.js
@@ -3,3 +3,15 @@
 //= require bootstrap
 //= require edit_common
 //= require qawechat/jweixin-1.0.0
+
+(function($) {
+  $.extend({
+    toastMsg: function(msg) {
+      var toastNode = $("<div class='prompt-error'><span>" + msg + "xxxx</span></div>");
+      $("body").append(toastNode);
+      toastNode.show().delay(3 * 1000).fadeOut(function() {
+        toastNode.remove();
+      });
+    }
+  });
+})(jQuery);

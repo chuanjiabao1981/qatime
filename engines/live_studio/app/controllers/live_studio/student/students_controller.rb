76af22c1b45a235ab0570af8189e64fa81a3c80b
liugang <501225658@qq.com>
2017-04-25 10:59:56 +0800

    QT-1062 老师学生课程表改版

--- engines/live_studio/app/controllers/live_studio/student/students_controller.rb
+++ engines/live_studio/app/controllers/live_studio/student/students_controller.rb
@@ -2,6 +2,7 @@ require_dependency "live_studio/student/base_controller"
 
 module LiveStudio
   class Student::StudentsController < Student::BaseController
+    layout 'v1/home'
 
     def schedules
       course_lessons = @student.live_studio_lessons

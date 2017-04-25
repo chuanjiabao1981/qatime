3256c88aa748a3841cee36afa4a704fe07b6b5e2
xinshuaifeng <xinshuaifeng@126.com>
2017-04-25 13:24:56 +0800

    QT-1059 学生我的直播课

--- engines/live_studio/app/controllers/live_studio/student/courses_controller.rb
+++ engines/live_studio/app/controllers/live_studio/student/courses_controller.rb
@@ -2,11 +2,10 @@ require_dependency "live_studio/student/base_controller"
 
 module LiveStudio
   class Student::CoursesController < Student::BaseController
-    layout 'student_home_new'
+    layout 'v1/home'
 
     def index
-      @courses = LiveService::StudentLiveDirector.new(@student).courses(params)
-      @courses = @courses.includes(:lessons).paginate(page: params[:page])
+      @courses = @student.live_studio_bought_courses.where(status: filter_status).includes(:lessons).paginate(page: params[:page])
     end
 
     def show
@@ -19,12 +18,8 @@ module LiveStudio
 
     private
 
-    def filter_patams
-      # status参数和cate参数保留一个
-      # 使用分类查询不能使用状态查询
-      @filter_patams = params.slice(:cate, :status)
-      @filter_patams.delete(:status) if @filter_patams[:cate]
-      @filter_patams
+    def filter_status
+      LiveStudio::Course.statuses[params[:status] || 'published']
     end
   end
 end

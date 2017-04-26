7c7f7b049fd0df1e129da9c452047edefa12c2c8
liugang <501225658@qq.com>
2017-04-26 20:39:25 +0800

    QT-1071 首页城市列表不显示未加盟的城市

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,7 +5,10 @@ class City < ActiveRecord::Base
   has_many :schools, inverse_of: :city, dependent: :destroy
   has_many :workstations
 
+  belongs_to :workstation
   belongs_to :province
+
+  scope :has_default_workstation, -> { where.not(workstation_id: nil) }
 
   def name_with_province(delimiter = '')
     "#{province.try(:name)}#{delimiter}#{name}"
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,6 @@
 class Province < ActiveRecord::Base
   has_many :cities
+
+  scope :has_default_workstation, -> { joins(:cities).where.not(cities: {workstation_id: nil}).distinct }
+
 end

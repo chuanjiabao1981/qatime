bf617e981bd981bf8e6d2ad293ce3502712cc8e8
xinshuaifeng <xinshuaifeng@126.com>
2017-04-28 14:29:55 +0800

    测试错误

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -4,7 +4,7 @@ class TagTest < ActiveSupport::TestCase
   test 'category of' do
     assert_equal 19, Tag.category_of.count, "不过滤标签查询错误"
     assert_equal 14, Tag.category_of('初三').count, "标签查询数据错误"
-    assert_equal 11, Tag.category_of('语文', '初三').count, "多个标签数据返回错误"
+    assert_equal 12, Tag.category_of('语文', '初三').count, "多个标签数据返回错误"
     assert_equal 19, Tag.category_of('').count, "空分类数据查询错误"
   end
 

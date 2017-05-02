05dd73a87dfcc9fcf3c5e91d11ad0007eb69cc89
liugang <501225658@qq.com>
2017-05-02 15:19:34 +0800

    城市搜索添加province_id条件

--- app/apis/v1/app_constants.rb
+++ app/apis/v1/app_constants.rb
@@ -36,12 +36,16 @@ module V1
       desc '获取城市列表'
       params do
         optional :scope, type: String, desc: '是否加盟: has_default_workstation;', values: %w[has_default_workstation]
+        optional :province_id, type: Integer, desc: '省ID'
       end
       get :cities do
         if params[:scope] && params[:scope] == 'has_default_workstation'
           cities = ::City.has_default_workstation
         else
           cities = ::City.all
+        end
+        if params[:province_id]
+          cities = cities.where(province_id: params[:province_id])
         end
         present cities, with: Entities::City
       end

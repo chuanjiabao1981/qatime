45de49a523b566d6de6cd7b61546a8e83bf01b33
liugang <501225658@qq.com>
2017-05-02 10:29:36 +0800

    QT-1084 城市api 添加scope过滤未加盟城市

--- app/apis/v1/app_constants.rb
+++ app/apis/v1/app_constants.rb
@@ -21,14 +21,28 @@ module V1
       end
 
       desc '获取省份列表'
+      params do
+        optional :scope, type: String, desc: '是否加盟: has_default_workstation;', values: %w[has_default_workstation]
+      end
       get :provinces do
-        provinces = ::Province.all
+        if params[:scope] && params[:scope] == 'has_default_workstation'
+          provinces = ::Province.has_default_workstation
+        else
+          provinces = ::Province.all
+        end
         present provinces, with: Entities::Province
       end
 
       desc '获取城市列表'
+      params do
+        optional :scope, type: String, desc: '是否加盟: has_default_workstation;', values: %w[has_default_workstation]
+      end
       get :cities do
-        cities = ::City.all
+        if params[:scope] && params[:scope] == 'has_default_workstation'
+          cities = ::City.has_default_workstation
+        else
+          cities = ::City.all
+        end
         present cities, with: Entities::City
       end
 

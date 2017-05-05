59811787b0d0fee7472b3178036eb419b7663eee
liugang <501225658@qq.com>
2017-05-05 17:41:29 +0800

    QT-1099 学生我的视频课 已购 免费逻辑修改

--- engines/live_studio/app/controllers/live_studio/student/video_courses_controller.rb
+++ engines/live_studio/app/controllers/live_studio/student/video_courses_controller.rb
@@ -4,12 +4,10 @@ module LiveStudio
   class Student::VideoCoursesController < Student::BaseController
     layout 'v1/home'
 
-    # GET /student/video_courses
     def index
-      @video_courses = @student.live_studio_video_courses
-      ticket_type = params[:cate] == 'tasting' ? "LiveStudio::TasteTicket" : "LiveStudio::BuyTicket"
-      @video_courses = @video_courses.where("live_studio_tickets.type = ?", ticket_type)
-      @video_courses = @video_courses.paginate(page: params[:page])
+      params[:cate] ||= 'charge'
+      course_data = LiveService::StudentLiveDirector.new(@student)
+      @video_courses = course_data.video_courses(sell_type: params[:cate]).paginate(page: params[:page])
     end
   end
 end

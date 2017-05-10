8be4b90664e6e4795f772584d7bd086989ea6816
liugang <501225658@qq.com>
2017-05-10 21:03:55 +0800

    QT-1113 新课发布支持视频课和1对1数据

---
+++ app/apis/entities/live_studio/rank_all_course.rb
@@ -0,0 +1,20 @@
+module Entities
+  module LiveStudio
+    class RankAllCourse < Grape::Entity
+      expose :id
+      expose :name
+      expose :product_type do |course|
+        course.model_name
+      end
+      expose :product, if: lambda { |object, options| object.is_a?(::LiveStudio::Course) } do |instance, options|
+        ::Entities::LiveStudio::Course.represent instance
+      end
+      expose :product, if: lambda { |object, options| object.is_a?(::LiveStudio::InteractiveCourse) } do |instance, options|
+        ::Entities::LiveStudio::InteractiveCourse.represent instance
+      end
+      expose :product, if: lambda { |object, options| object.is_a?(::LiveStudio::VideoCourse) } do |instance, options|
+        ::Entities::LiveStudio::VideoCourse.represent instance
+      end
+    end
+  end
+end
--- app/services/live_service/rank_manager.rb
+++ app/services/live_service/rank_manager.rb
@@ -7,6 +7,12 @@ module LiveService
 
       private
 
+      # 所有课程 最新发布
+      def all_published_rank(options)
+        options[:limit] ||= 4
+        all_base_filter(options).sort_by{|x| x.published_at || Time.new(2000) }.reverse.to(options[:limit] - 1)
+      end
+
       # 发布时间排行
       def published_rank(options)
         base_filter(options).reorder('published_at desc')
@@ -21,6 +27,19 @@ module LiveService
         filter = LiveStudio::Course.for_sell
         filter = filter.where(city_id: options[:city_id]) if options[:city_id].present?
         filter
+      end
+
+      def all_base_filter(options)
+        options[:limit] ||= 4
+        courses = LiveStudio::Course.for_sell
+        interactive_courses = LiveStudio::InteractiveCourse.for_sell
+        video_courses = LiveStudio::VideoCourse.for_sell
+        if options[:city_id].present?
+          courses = courses.where(city_id: options[:city_id])
+          interactive_courses = interactive_courses.where(city_id: options[:city_id])
+          video_courses = video_courses.where(city_id: options[:city_id])
+        end
+        courses.limit(options[:limit]).to_a + interactive_courses.limit(options[:limit]).to_a + video_courses.limit(options[:limit]).to_a
       end
     end
   end

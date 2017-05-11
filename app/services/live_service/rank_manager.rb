0538d2e906d4802c9f3ed7a30b35ed8471e62324
liugang <501225658@qq.com>
2017-05-11 10:31:30 +0800

    QT-1113 新课发布 默认排序

--- app/services/live_service/rank_manager.rb
+++ app/services/live_service/rank_manager.rb
@@ -10,6 +10,7 @@ module LiveService
       # 所有课程 最新发布
       def all_published_rank(options)
         options[:limit] ||= 4
+        options[:sort_by] ||= 'published_at desc'
         all_base_filter(options).sort_by{|x| x.published_at || Time.new(2000) }.reverse.to(options[:limit] - 1)
       end
 
@@ -31,9 +32,10 @@ module LiveService
 
       def all_base_filter(options)
         options[:limit] ||= 4
-        courses = LiveStudio::Course.for_sell
-        interactive_courses = LiveStudio::InteractiveCourse.for_sell
-        video_courses = LiveStudio::VideoCourse.for_sell
+        options[:sort_by] ||= 'id desc'
+        courses = LiveStudio::Course.for_sell.order(options[:sort_by])
+        interactive_courses = LiveStudio::InteractiveCourse.for_sell.order(options[:sort_by])
+        video_courses = LiveStudio::VideoCourse.for_sell.order(options[:sort_by])
         if options[:city_id].present?
           courses = courses.where(city_id: options[:city_id])
           interactive_courses = interactive_courses.where(city_id: options[:city_id])

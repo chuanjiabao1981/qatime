2d5d0b48e3a79be04d5f88043dda432422e04d6d
liugang <501225658@qq.com>
2017-05-19 17:18:58 +0800

    课程表测试修改

--- test/integration/live_studio/schedules_test.rb
+++ test/integration/live_studio/schedules_test.rb
@@ -78,7 +78,7 @@ module LiveStudio
       find(".cell-#{today.month}-#{today.day}").click
       sleep(3)
       assert_equal 3, all("#calendar_data li").size, '数据没有出现'
-      assert page.has_content?("课程日历")
+      assert page.has_content?("全部课程")
       assert page.has_content?("未上课")
       assert page.has_content?("已上课")
       visit teacher_path(teacher)
@@ -88,30 +88,10 @@ module LiveStudio
       find(".cell-#{today.month}-#{today.day}").click
       sleep(3)
       assert_equal 2, all("#calendar_data li").size, '数据没有出现'
-      assert page.has_content?("课程日历")
+      assert page.has_content?("全部课程")
       assert page.has_content?("未上课")
       assert page.has_content?("已上课")
       new_logout_as(@admin)
-    end
-
-    test 'teacher schedules tips' do
-      teacher = users(:teacher_with_chat_account)
-      log_in_as(teacher)
-      el = find('.btn-schedule')
-      page.driver.browser.action.move_to(el.native).perform
-      find('.teacher-schedule-1').click
-      assert page.has_content?('课程列表')
-      new_logout_as(teacher)
-    end
-
-    test 'student schedules tips' do
-      student = users(:student_one_with_course)
-      log_in_as(student)
-      el = find('.btn-schedule')
-      page.driver.browser.action.move_to(el.native).perform
-      find('.teacher-schedule-1',match: :first).click
-      assert page.has_content?('课程列表')
-      new_logout_as(student)
     end
   end
 end

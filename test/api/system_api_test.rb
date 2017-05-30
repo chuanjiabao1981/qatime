f3f7c013e41cb893d8c5b1c10deec5ddc8bd30ab
liugang <501225658@qq.com>
2017-05-30 18:23:47 +0800

    QT-373 api test rubocop 风格 修正

--- test/api/system_api_test.rb
+++ test/api/system_api_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
-
   test 'check_update api' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.1', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.1', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']
@@ -10,7 +9,7 @@ class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
   end
 
   test 'check update api enforce true' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.1', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.1', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']
@@ -18,7 +17,7 @@ class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
   end
 
   test 'check update api enforce false' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.2', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.2', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']

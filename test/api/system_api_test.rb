b63ff5fd87aea99112d799221ba82bddba74ad18
xinshuaifeng <xinshuaifeng@126.com>
2017-05-30 20:52:48 +0800

    QT-373 测试警告

--- test/api/system_api_test.rb
+++ test/api/system_api_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
 
   test 'check_update api' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.1', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.1', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']
@@ -10,7 +10,7 @@ class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
   end
 
   test 'check update api enforce true' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.1', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.1', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']
@@ -18,7 +18,7 @@ class Qatime::SystemApiTest < ActionDispatch::IntegrationTest
   end
 
   test 'check update api enforce false' do
-    get "/api/v1/system/check_update", {category: 'teacher_live', version: '0.0.2', platform: 'windows'}
+    get "/api/v1/system/check_update", params: { category: 'teacher_live', version: '0.0.2', platform: 'windows' }
     assert_response :success
     res = JSON.parse(response.body)
     assert_equal 1, res['status']

1bacd5188872cf6dc494891c82747de7e4b73bec
xinshuaifeng <xinshuaifeng@126.com>
2017-05-31 19:00:09 +0800

    QT-373 代码格式化

--- app/models/course_issue.rb
+++ app/models/course_issue.rb
@@ -2,19 +2,17 @@ class CourseIssue < Topic
   include QaIssue
   include QaCommon
 
-
-  belongs_to :customized_course,counter_cache: true
-  has_many :course_issue_replies,foreign_key: "topic_id",dependent: :destroy do
+  belongs_to :customized_course, counter_cache: true
+  has_many :course_issue_replies, foreign_key: "topic_id", dependent: :destroy do
     include QaPageNum
 
-    def build(attributes={})
-      attributes[:customized_course_id]       = proxy_association.owner.customized_course_id
+    def build(attributes = {})
+      attributes[:customized_course_id] = proxy_association.owner.customized_course_id
       super attributes
     end
 
     def page_num(o)
-      _page_num(o,by: Reply.order_column,order: Reply.order_type,per: Reply.per_page)
+      _page_num(o, by: Reply.order_column, order: Reply.order_type, per: Reply.per_page)
     end
   end
-
 end

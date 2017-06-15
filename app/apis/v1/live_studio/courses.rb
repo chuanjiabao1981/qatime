56ceb45cf40c85d0e6a397ee505d6a40a999d483
liugang <501225658@qq.com>
2017-06-15 20:31:37 +0800

    QT-1244 课程表显示本周数据

--- app/apis/v1/live_studio/courses.rb
+++ app/apis/v1/live_studio/courses.rb
@@ -73,11 +73,16 @@ module V1
               }
             end
             params do
+              optional :week, type: String, desc: '本周内: 2016-10-01 返回本周7天的数据'
               optional :month, type: String, desc: '月份: 2016-10-01 该值为空则默认返回当月数据'
               optional :state, type: String, desc: '课程状态:未上课 已完成 不传则默认返回全部', values: %w(unclosed closed)
             end
             get 'schedule' do
-              arr = LiveService::CourseDirector.courses_by_month(current_user, params[:month], params[:state])
+              if params[:week].present?
+                arr = LiveService::CourseDirector.courses_by_week(current_user, params[:week], params[:state])
+              else
+                arr = LiveService::CourseDirector.courses_by_month(current_user, params[:month], params[:state])
+              end
               present arr, with: Entities::LiveStudio::Schedule, type: :schedule
             end
           end

3db2f7a7d5ff7699a7957986a72e05eb7e179498
xinshuaifeng <xinshuaifeng@126.com>
2017-06-19 16:42:18 +0800

    QT-1247 课程表代码恢复

--- engines/live_studio/app/controllers/live_studio/student/students_controller.rb
+++ engines/live_studio/app/controllers/live_studio/student/students_controller.rb
@@ -5,15 +5,13 @@ module LiveStudio
     layout 'v1/home'
 
     def schedules
-      course_lessons = @student.live_studio_ticket_items.
-                                where(class_date: Time.now.beginning_of_week..(Time.now.end_of_week + 1.day)).
-                                where(target_type: 'LiveStudio::Lesson').includes(:ticket, target: :course)
-      interactive_course_lessons = @student.live_studio_ticket_items.where(target_type: 'LiveStudio::InteractiveLesson').includes(:ticket, target: :interactive_course)
+      course_lessons = @student.live_studio_lessons
+      interactive_course_lessons = @student.live_studio_interactive_lessons
 
       @wait_lessons = course_lessons.unclosed.to_a + interactive_course_lessons.unclosed.to_a
       @close_lessons = course_lessons.already_closed.to_a + interactive_course_lessons.already_closed.to_a
-      @wait_lessons = @wait_lessons.sort_by { |x| x.start_at }.reverse
-      @close_lessons = @close_lessons.sort_by { |x| x.start_at }
+      @wait_lessons = @wait_lessons.sort_by {|x| x.start_at }.reverse
+      @close_lessons = @close_lessons.sort_by {|x| x.start_at }
     end
 
     def settings
@@ -30,5 +28,6 @@ module LiveStudio
       student_data = LiveService::StudentLiveDirector.new(@student)
       @taste_records = student_data.taste_records.paginate(page: params[:page])
     end
+
   end
 end

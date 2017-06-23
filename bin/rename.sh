e9da8a88b6c7c08569910124cea4134be8545665
xinshuaifeng <xinshuaifeng@126.com>
2017-06-23 18:10:06 +0800

    QT-1234 文件重命名

--- bin/rename.sh
+++ bin/rename.sh
@@ -2,10 +2,10 @@
 cd ../app/assets/images/
 
 for filename in `ls */*/*X*.png`; do
-  new_name=`echo $filename | sed 's/160X100/small/'`
-  new_name=`echo $new_name | sed 's/320X200/list/'`
-  new_name=`echo $new_name | sed 's/480X300/info/'`
-  new_name=`echo $new_name | sed 's/800X500/app_info/'`
+  new_name=`echo $filename | sed 's/160X100/small_default/'`
+  new_name=`echo $new_name | sed 's/320X200/list_default/'`
+  new_name=`echo $new_name | sed 's/480X300/info_default/'`
+  new_name=`echo $new_name | sed 's/800X500/app_info_default/'`
 
   echo "rename file ${filename} ===> ${new_name}"
   mv $filename $new_name

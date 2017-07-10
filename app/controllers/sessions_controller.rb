ca8934774b7506d51b0f10af5ac43b763cfa7d8c
liugang <501225658@qq.com>
2017-07-10 16:13:36 +0800

    QT-1333 登录页面记住账号功能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,22 @@
 class SessionsController < ApplicationController
-  before_filter 'already_signin' ,only: [:new]
+  before_filter 'already_signin', only: [:new]
   layout 'application_login'
+
   def new
     @user = User.new
+    @user.login_account = cookies[:remember_account].presence
   end
 
   def create
-    # @user = User.find_by(email: params[:user][:email])
     @user = User.find_by_login_account(params[:user][:login_account])
     if @user && @user.authenticate(params[:user][:password])
       sign_in(@user)
+      if params[:remember_account] == '1'
+        cookies.permanent[:remember_account] = params[:user][:login_account]
+      end
       flash[:info] = "欢迎登录!"
       redirect_to params[:redirect_url].blank? ? after_sign_in_path : params[:redirect_url]
     else
-      # @user = User.new(email: @user.try(:email))
       @user = User.new(login_mobile: @user.try(:login_account))
       flash.now[:warning] = "用户名或密码错误!"
       render 'new'

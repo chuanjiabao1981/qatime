616d2e55400ab600ef500ebcb0dcbc2d9a8eeea7
xinshuaifeng <xinshuaifeng@126.com>
2017-07-13 15:01:49 +0800

    QT-1337 语法错误

--- engines/live_studio/app/models/live_studio/lesson.rb
+++ engines/live_studio/app/models/live_studio/lesson.rb
@@ -9,7 +9,7 @@ module LiveStudio
     attr_accessor :start_time_hour, :start_time_minute, :_update
     BEAT_STEP = 10 # 心跳频率/秒
 
-    delegate :teacher_percentage, :publish_percentage, :base_price, :workstation, to: :course
+    delegate :teacher_percentage, :publish_percentage, :base_price, :workstation, :board_channel, to: :course
     delegate :channels, to: :course
 
     enum replay_status: {

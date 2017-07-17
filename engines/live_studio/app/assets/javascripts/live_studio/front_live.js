a620a0e653ad3306d82b76f0d5c15d6bd5b79045
liugang <501225658@qq.com>
2017-07-17 14:53:40 +0800

    QT-1246 就播放页面js 保留

---
+++ engines/live_studio/app/assets/javascripts/live_studio/front_live.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.Jcrop
+//= require bootstrap
+//= require summernote
+//= require jquery.form
+//= require jquery.menu-aim
+//= require chosen-jquery
+//= require canvas-to-blob.min
+//= require fancybox
+//= require editable/bootstrap-editable
+//= require editable/rails
+//= require bootstrap-select.min
+//= require defaults-zh_CN
+//= require jquery.qqFace.js
+//= require live_studio/nep.min
+//= require live_studio/Web_SDK_Base_v2.4.0
+//= require live_studio/Web_SDK_NIM_v2.4.0
+//= require live_studio/chat
+//= require util
+//= require jedate
+//= require jquery.jedate
+//= require weixin-audio
+//= require v1/home

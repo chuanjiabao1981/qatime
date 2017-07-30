10d73e21a3c8bd9a6141dbf97c28dcfe8f29984d
liugang <501225658@qq.com>
2017-07-30 15:04:30 +0800

    QT-1368 学生免费课购买权限

--- app/models/permissions/student_permission.rb
+++ app/models/permissions/student_permission.rb
@@ -158,6 +158,7 @@ module Permissions
       allow 'live_studio/student/interactive_courses', [:index, :show]
       allow 'live_studio/student/video_courses', [:index]
       allow 'live_studio/student/customized_groups', [:index]
+      allow 'live_studio/customized_groups', [:index, :show, :for_free]
       allow 'live_studio/courses', [:index, :show, :for_free]
       allow 'live_studio/lessons', [:show, :play, :videos]
 

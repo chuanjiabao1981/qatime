58f00fe084cf2b41e7a604b0c01a8d1f2b085b4f
xinshuaifeng <xinshuaifeng@126.com>
2017-08-02 18:17:43 +0800

    QT-1389 结账测试

--- app/services/business_service/billing.rb
+++ app/services/business_service/billing.rb
@@ -12,10 +12,11 @@ module BusinessService
         @target = target
       end
 
-      def execute
+      def execute!
         return unless _check_lesson
         billing = Payment::Billings::LessonBilling.create(target: @target, from_user: @target.teacher, created_at: @created_at)
         @target.ticket_items.billingable.map {|item| execute_item(billing, item) }
+        @target.complete! if @target.ticket_items.billingable.blank?
       rescue StandardError => e
         _billing_fail!(e)
         raise e
@@ -25,16 +26,16 @@ module BusinessService
 
       def execute_item(parent, item)
         item.with_lock do
-          ticket = ticket_item.ticket
+          ticket = item.ticket
           item_billing = _item_billing(parent, ticket)
           # 系统服务费收入
           system_fee_item!(item_billing)
           # 教师收入
-          teacher_money_item!(item_billing, _target_teacher)
+          teacher_money_item!(item_billing)
           # 发行商收入
-          publish_money_item!(item_billing, _target_workstation)
+          publish_money_item!(item_billing)
           # 销售分销商收入
-          sell_money_item!(item_billing, _ticket_seller(ticket))
+          sell_money_item!(item_billing)
           # 系统分成收入
           platform_money_item!(item_billing)
           # 结账完成后购买记录修改状态，避免重复结账
@@ -74,12 +75,77 @@ module BusinessService
 
       # 销售工作站
       def _ticket_seller(ticket)
-        return @ticket_seller if @ticket_seller.present?
-        @ticket_seller = ticket.seller || Workstation.default
+        ticket.seller || Workstation.default
       end
 
       def _item_billing(parent, ticket)
         Payment::Billings::TicketItemBilling.create!(target: @target, from_user: _target_teacher, parent: parent, ticket: ticket)
+      end
+
+      def _check_lesson
+        true
+      end
+
+      # 系统服务费
+      def system_fee_item!(billing)
+        item = Payment::SystemFeeItem.create!(
+          billing: billing,
+          cash_account: _system_account,
+          owner: CashAdmin.current!,
+          amount: billing.base_fee,
+          quantity: 1,
+          price: @target.base_price,
+          duration: @target.duration_hours * 60
+        )
+        _cash_transfer(_system_account, billing.base_fee, item)
+      end
+
+      # 销售账单项
+      def sell_money_item!(billing)
+        item = Payment::SellPercentItem.create!(
+          billing: billing,
+          cash_account: _ticket_seller(billing.ticket).cash_account,
+          owner: _ticket_seller(billing.ticket),
+          amount: billing.sell_money,
+          percent:  billing.sell_percentage
+        )
+        _cash_transfer(_ticket_seller(billing.ticket).cash_account, billing.sell_money, item)
+      end
+
+      # 平台分成账单项
+      def platform_money_item!(billing)
+        item = Payment::PlatformPercentItem.create!(
+          billing: billing,
+          cash_account: _system_account,
+          owner: CashAdmin.current!,
+          amount: billing.platform_money,
+          percent:  billing.platform_percentage
+        )
+        _cash_transfer(_system_account, billing.platform_money, item)
+      end
+
+      # 发行账单项
+      def publish_money_item!(billing)
+        item = Payment::PublishPercentItem.create!(
+          billing: billing,
+          cash_account: _target_workstation.cash_account,
+          owner: _target_workstation,
+          amount: billing.publish_money,
+          percent: @target.publish_percentage
+        )
+        _cash_transfer(_target_workstation.cash_account, billing.publish_money, item)
+      end
+
+      # 教师分成收入
+      def teacher_money_item!(billing)
+        item = Payment::TeacherMoneyItem.create(
+          billing: billing,
+          cash_account: _target_teacher.cash_account,
+          owner: _target_teacher,
+          amount: billing.teacher_money,
+          percent: @target.teacher_percentage
+        )
+        _cash_transfer(_target_teacher.cash_account, billing.teacher_money, item)
       end
     end
   end
--- app/services/business_service/billing/event_lesson.rb
+++ app/services/business_service/billing/event_lesson.rb
@@ -2,6 +2,17 @@ module BusinessService
   module Billing
     # 收费课程结账
     class EventLesson < Base
+      private
+      def _check_lesson
+        # 检查课程老师信息
+        @target.teacher = @target.group.teacher unless @target.teacher.present?
+        # 检查课程时长
+        @target.real_time = @target.live_sessions.sum(:duration) unless @target.real_time.to_i > 0
+        @target.save
+        # 结账前确保教师有资金账户
+        @target.teacher.cash_account!
+        @target.waiting_billing?
+      end
     end
   end
 end

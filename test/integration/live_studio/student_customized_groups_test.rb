20869ccf38e18af7db7f55f6e6f4e5379aec865d
liugang <501225658@qq.com>
2017-08-03 14:36:47 +0800

    QT-1401 专属课观看页测试

--- test/integration/live_studio/student_customized_groups_test.rb
+++ test/integration/live_studio/student_customized_groups_test.rb
@@ -30,5 +30,17 @@ module LiveStudio
       click_on '已结束'
       assert page.has_link? '我的专属课completed'
     end
+
+    test "visit student customized group play page" do
+      customized_group = live_studio_groups(:teaching_group_for_student_view1)
+      visit live_studio.customized_group_path(customized_group)
+      click_on '开始学习'
+
+      assert page.has_content?(customized_group.current_event.name), '直播课程名称未显示'
+      assert page.has_content?(customized_group.name), '直播课程名称未显示'
+      assert page.has_link?(customized_group.teacher.name), '老师未显示'
+      assert page.has_content? '线上直播'
+      assert page.has_content? '线下讲课'
+    end
   end
 end

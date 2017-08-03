00530efc2152d9bd135aaedeb50470b2eb677e04
liugang <501225658@qq.com>
2017-08-03 15:37:48 +0800

    QT-1402 老师查看-课程详情页（直播/一对一/专属课）增加发布新公告功能

--- test/integration/live_studio/teacher_announcements_test.rb
+++ test/integration/live_studio/teacher_announcements_test.rb
@@ -32,5 +32,35 @@ module LiveStudio
         assert page.has_content? '测试公告测试公告测试公告测试公告'
       end
     end
+
+    test 'teacher course show page announcement' do
+      course = @teacher.live_studio_courses.teaching.first
+      visit live_studio.course_path(course)
+      assert page.has_link? '发布公告'
+      click_on '发布公告', match: :first
+      sleep(1)
+      fill_in :announcement_content, with: '测试公告啊测试公告啊测试公告啊'
+      assert_difference "LiveStudio::Announcement.count", 1, "发布公告失败" do
+        click_on "发布"
+      end
+    end
+
+    test 'teacher interactive show page announcement' do
+      teacher1 = users(:teacher1)
+      new_logout_as(@teacher)
+      new_log_in_as(teacher1)
+
+      course = teacher1.live_studio_interactive_courses.teaching.first
+      visit live_studio.interactive_course_path(course)
+      assert page.has_link? '发布公告'
+      click_on '发布公告', match: :first
+      sleep(1)
+      fill_in :announcement_content, with: '测试公告啊测试公告啊测试公告啊'
+      assert_difference "LiveStudio::Announcement.count", 1, "发布公告失败" do
+        click_on "发布"
+      end
+      new_logout_as(teacher1)
+      new_log_in_as(@teacher)
+    end
   end
 end

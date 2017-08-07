1bf423176050c6138352ad1052771f12349688e1
xinshuaifeng <xinshuaifeng@126.com>
2017-08-07 17:05:58 +0800

    QT-1411 使用redis顺序执行直播开始结束接口

---
+++ lib/qatime/util.rb
@@ -0,0 +1,18 @@
+module Qatime
+  # 工具类
+  module Util
+    # 顺序执行
+    def self.sequence_exec(key, t)
+      t || Time.now.to_i
+      return if safe_cache(key, t) > t
+      yield if block_given?
+    end
+
+    # 安全缓存，用于缓存时间戳
+    def self.safe_cache(key, value)
+      last = Redis.current.get(key)
+      Redis.current.setex(key, 5.minutes, value) if last.to_i < value
+      last || value
+    end
+  end
+end

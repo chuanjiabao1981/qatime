d227fb917487d658a9aa81c4063960f1ef456c0a
xinshuaifeng <xinshuaifeng@126.com>
2017-09-04 17:43:05 +0800

    QT-1458 课间上传时间记录

--- app/apis/entities/resource/file.rb
+++ app/apis/entities/resource/file.rb
@@ -1,12 +1,17 @@
 module Entities
   module Resource
     class File < Grape::Entity
+      format_with(:local_timestamp, &:to_i)
+
       expose :id
       expose :name
       expose :type
       expose :file_size
       expose :ext_name
       expose :file_url
+      with_options(format_with: :local_timestamp) do
+        expose :created_at
+      end
     end
   end
 end

9d4657afc08d570a52c95a50a315beeb2afb799e
liugang <501225658@qq.com>
2017-09-05 11:27:25 +0800

    API测试修改

--- test/api/live_studio/student_schedule_api_test.rb
+++ test/api/live_studio/student_schedule_api_test.rb
@@ -11,7 +11,7 @@ class Qatime::StudentScheduleAPITest < ActionDispatch::IntegrationTest
     assert_response :success
     data = JSON.parse(response.body)['data']
     assert data.class == Array
-    assert_equal 1, data.first['lessons'].count, '返回课程数量不对'
+    assert_equal 2, data.first['lessons'].count, '返回课程数量不对'
     assert data.first['lessons'].first.key?('taste'), '试听标识未出现'
     return_date = data.first['date'].to_date
     assert return_date >= Time.now.beginning_of_month.to_date && return_date <= Time.now.end_of_month.to_date, '返回数据日期不正确'
@@ -20,7 +20,7 @@ class Qatime::StudentScheduleAPITest < ActionDispatch::IntegrationTest
     assert_response :success
     data = JSON.parse(response.body)['data']
     assert data.class == Array
-    assert_equal 2, data.first['lessons'].count, '返回课程数量不对'
+    assert_equal 1, data.first['lessons'].count, '返回课程数量不对'
     assert data.first['lessons'].first.key?('taste'), '试听标识未出现'
     return_date = data.first['date'].to_date
     assert return_date >= Time.now.beginning_of_week.to_date && return_date <= Time.now.end_of_week.to_date, '返回数据日期不正确'

7551c05416125bb0ac87d58987f8ef6098b84821
xinshuaifeng <xinshuaifeng@126.com>
2017-11-01 17:17:57 +0800

    QT-1493 上课安排接口数据丰富

--- app/apis/entities/live_studio/lesson_schedule.rb
+++ app/apis/entities/live_studio/lesson_schedule.rb
@@ -1,12 +1,20 @@
 module Entities
   module LiveStudio
     class LessonSchedule < Grape::Entity
+      format_with(:local_timestamp, &:to_i)
+
       expose :id
       expose :model_name
       expose :name
-      expose :start_at
       expose :course_id
       expose :course_model_name
+      expose :status
+      expose :publicizes do |lesson|
+        lesson.publicize.versions
+      end
+      with_options(format_with: :local_timestamp) do
+        expose :start_at
+      end
     end
   end
 end

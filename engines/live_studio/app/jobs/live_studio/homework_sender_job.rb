a55dc2ca0b1718d2fb697b81a6f96a58b0028333
xinshuaifeng <xinshuaifeng@126.com>
2017-11-09 17:19:07 +0800

    QT-1504 补发作业过期

--- engines/live_studio/app/jobs/live_studio/homework_sender_job.rb
+++ engines/live_studio/app/jobs/live_studio/homework_sender_job.rb
@@ -1,6 +1,6 @@
 module LiveStudio
   class HomeworkSenderJob < ActiveJob::Base
-    queue_as :default
+    queue_as :live_studio
 
     def perform(group_id, student_id)
       LiveStudio::CustomizedGroup.find(group_id).send_homeworks_to(student_id)
